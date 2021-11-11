9fb31fbf8f7e11ed3bb00eb4a1b6cdb18f46fb3d
minte9 <catalinprescure@gmail.com>
2021-11-11 21:22:31 +0200

    python-pages update

--- main/language/functions/definition.py
+++ main/language/functions/definition.py
@@ -1,11 +1,12 @@
-# python-pages/language/functions/def.py
-# python def.py
-
+# A function is a named sequence of statements.
+#
 # A function definition specifies the name and the statements.
+# Expression in parantheses is called the argument
+#
 # To end the function, you have to enter an empty line.
 # Statements inside a function don't run until the function is called
 
-def myfunc(a):
+def myfunc(a): 
     return a%2
 
 print (myfunc(3))
