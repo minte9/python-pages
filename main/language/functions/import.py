77f00d27b840af0e42150398922f628b81c4fe80
minte9 <catalinprescure@gmail.com>
2021-11-11 21:33:10 +0200

    python-pages update

---
+++ main/language/functions/import.py
@@ -0,0 +1,10 @@
+# Functions from module
+# To use a function from a module, you have to import it.
+
+import math
+
+print(math.pi)
+    # 3.141592653589793
+
+print(math.sin(math.pi/2))
+    # 1.0
