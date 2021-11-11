2345d1a721114be761d14f289151f9342c65af78
minte9 <catalinprescure@gmail.com>
2021-11-11 21:04:12 +0200

    python-pages update

---
+++ main/language/variables/operators.py
@@ -0,0 +1,9 @@
+# The + operator concatenates two strings.
+# The * operator performs repetitions on strings.
+
+s1 = 'Hello'
+s2 = 'World'
+
+print(s1 + ' ' + s2) # 'Hello World'
+
+print('Spam'*3) # 'SpamSpamSpam'
---
+++ main/language/variables/statements.py
@@ -0,0 +1,5 @@
+# When you type a statement, the interpreter executes it
+
+n = 17    # statement 1
+print(n)  # statement 2 
+    # 17
