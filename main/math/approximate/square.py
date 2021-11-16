968182cedc0542e548a3c39cb03f15235054b890
minte9 <catalinprescure@gmail.com>
2021-11-16 20:19:44 +0200

    python-pages update

---
+++ main/math/approximate/square.py
@@ -0,0 +1,33 @@
+# Newton's method for finding roots ...
+# is a root-finding algorithm to produce succesive better aproximations
+# 
+# Suppose that you want to know the square root of a. 
+# If you start with almost any estimate x ...
+# you can compute a better estimation with the following formula:
+#     y = (x + a/x) / 2
+# 
+# For example with a = 4, x = 3
+# the result is preatty close to correct answer 2
+# (3 + 4/3) / 2 = 2.16666666667
+#
+# If we repeat the process with the new estimate, it gets even closer
+
+
+def square(a, x):
+    while True:
+        y = (x + a/x) / 2
+        if (y == x): 
+            break
+        x = y
+        print(y)
+    return y
+
+y = square(4, 10)
+
+# 5.2
+# 2.9846153846153847
+# 2.1624107850911973
+# 2.006099040777959
+# 2.00000927130158
+# 2.000000000021489
+# 2.0
