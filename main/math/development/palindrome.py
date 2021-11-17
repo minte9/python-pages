e0ffb5c42f1c6caff6060e9c9836a1812edb0c5c
minte9 <catalinprescure@gmail.com>
2021-11-17 12:53:50 +0200

    python-pages update

---
+++ main/math/development/palindrome.py
@@ -0,0 +1,74 @@
+# Palindrom:
+# 
+# A palindrom is a word that is spelled the same backward and forward
+# Example: noon, redivider
+#
+# We use built-in function len() to check the string length
+
+
+# FIRST implementation:
+# -------------------------------------
+#   - define function
+
+def is_palindrome_v1(word):
+    return True
+
+assert is_palindrome_v1("abc") == True
+
+
+# SECOND:
+# -------------------------------------
+#   - test first/last
+
+def is_palindrome_v2(word):
+    first = word[0]
+    last = word[-1]
+    if (first != last):
+        return False
+    return True
+
+assert is_palindrome_v2("abca") == True
+assert is_palindrome_v2("abc") == False
+
+
+# THIRD:
+# -------------------------------------
+#   - test middle
+
+def is_palindrome_v2(word):
+    if (word[0] != word[-1]):
+        return False
+    if (len(word) > 2):
+        middle = word[1:-1]
+        return is_palindrome_v2(middle)
+    return True
+
+assert is_palindrome_v2("abcba") == True
+assert is_palindrome_v2("abca") == False
+
+
+# FINAL:
+# -------------------------------------
+#   - refactoring
+
+
+def first(word): 
+    return word[0]
+
+def last(word): 
+    return word[-1]
+
+def middle(word): 
+    return word[1:-1]
+
+def is_palindrome(word):
+    if (first(word) != last(word)):
+        return False
+    if (len(word) > 2):   
+        return is_palindrome(middle(word))
+    return True
+
+assert is_palindrome("noon") == True
+assert is_palindrome("redivider") == True
+assert is_palindrome("to")  == False
+assert is_palindrome("moomm") == False
