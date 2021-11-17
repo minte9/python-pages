36cdde55540583e9e5a3bde81245aae055e1ffaa
minte9 <catalinprescure@gmail.com>
2021-11-17 16:17:05 +0200

    python-pages update

--- main/collections/dictionaries/histogram.py
+++ main/collections/dictionaries/histogram.py
@@ -2,6 +2,12 @@
 # 
 # How many times each letter appears
 # To get a value use get(key, default) method
+#
+# Use for statement to traverse the keys of the dictionaries.
+# The keys are in no order.
+
+
+# Get histogram of a string
 
 def histogram(str):
     dictionary = dict()
@@ -21,3 +27,14 @@ assert histogram("google") == {'g': 2, 'o': 2, 'l': 1, 'e': 1}
 
 assert histogram("yahoo").get('o') == 2
 assert histogram("yahoo").get('o', 0) == 2
+
+
+# foreach key, print value
+
+h = histogram("google")
+for k in h:
+    print(k + ": " + str(h[k]))
+    # g: 2
+    # o: 2
+    # l: 1
+    # e: 1
