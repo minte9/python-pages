da508ae29072c9c11853c36e9bdc2afcc1c253a6
minte9 <catalinprescure@gmail.com>
2021-11-18 16:27:15 +0200

    python-pages update

---
+++ main/collections/tuples/tuple.py
@@ -0,0 +1,40 @@
+# A tuple is a comma separated values
+#
+# It is common to use parantheses, but not necessary
+#
+# A single value in parantheses is not a tuple
+# For a single element, include the final comma
+#
+# Tuples are different than lists, they are immutable.
+
+t = 'a', 'b', 'c'; 
+t = 'a',
+t = ('a', 'b', 'c')
+
+print(type(t)) 
+    # <class 'tuple'>
+
+
+# Not a tuple
+# -------------------------------------------
+t = ('a')
+print(type(t)) 
+    # <class 'str'>
+
+
+# Immutable
+# -------------------------------------------
+mylist = [1, 2, 3]
+mylist[0] = 4
+print(mylist)  # [4, 2, 3]
+
+mytuple = (1, 2, 3)
+# mytuple[0] = 4
+    # TypeError: 'tuple' object does not support item assignment
+
+
+# Replace
+# -------------------------------------------
+# You can't modify the elements, ...
+# but you can replace one tuple with another.
+# ---------------------------------------------
