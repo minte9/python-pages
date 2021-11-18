b9a473dff3e5e9f56f38e5820ea3ea49b7b6145a
minte9 <catalinprescure@gmail.com>
2021-11-18 16:32:26 +0200

    python-pages update

--- main/collections/tuples/tuple.py
+++ main/collections/tuples/tuple.py
@@ -6,31 +6,30 @@
 #
 # Tuples are different than lists, they are immutable.
 
-# CLASS
+# CLASS type
+# -------------------------------------------
 t = 'a', 'b', 'c'; 
 t = 'a',
 t = ('a', 'b', 'c')
 
-print(type(t)) 
-    # <class 'tuple'>
+print(type(t)) # <class 'tuple'>
 
 
 # NOT a tuple
 # -------------------------------------------
 t = ('a')
-print(type(t)) 
-    # <class 'str'>
+print(type(t)) # <class 'str'>
 
 
-# IMUTTABLE
+# IMMUTABLE
 # -------------------------------------------
 mylist = [1, 2, 3]
 mylist[0] = 4
 print(mylist)  # [4, 2, 3]
 
 mytuple = (1, 2, 3)
-# mytuple[0] = 4
-    # TypeError: 'tuple' object does not support item assignment
+# mytuple[0] = 4 
+#   # TypeError: 'tuple' object does not support item assignment
 
 
 # REPLACE
@@ -40,4 +39,5 @@ mytuple = (1, 2, 3)
 # ---------------------------------------------
 t = ('a', 'b', 'c', 'd', 'e')
 t = ('A',) + t[2:]
-print(t)  # ('A', 'c', 'd', 'e')
+print(t) 
+    # ('A', 'c', 'd', 'e')
