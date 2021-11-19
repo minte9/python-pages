1c0b973bfaac8eef7d9cb19145861b31ee2eda77
minte9 <catalinprescure@gmail.com>
2021-11-19 13:07:37 +0200

    python-pages update

--- main/class/methods/init.py
+++ main/class/methods/init.py
@@ -7,23 +7,8 @@ class Time:
         self.min = min
         self.sec = sec
 
-    def print(self):
-        print('%.2d:%.2d:%.2d' % (self.hour, self.min, self.sec))
-
-time = Time(9, 30) # parameters are optional
-time.print() 
-    # 09:30:00
-
-class Time:
-    def __init__(self, hour=0, min=0, sec=0):
-        self.hour = hour
-        self.min = min
-        self.sec = sec
-
     def __str__(self):
         return '%.2d:%.2d:%.2d' % (self.hour, self.min, self.sec)
 
 time = Time(9, 30)
-print(time) 
-    # 09:30:00
-    # print invokes __str___
+print(time) # 09:30:00
