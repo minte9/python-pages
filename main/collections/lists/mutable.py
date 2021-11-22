dca3fb3a6da1c4ee79617f326fc9bf8b64a01f81
minte9 <catalinprescure@gmail.com>
2021-11-22 16:45:46 +0200

    python-pages update

--- main/collections/lists/mutable.py
+++ main/collections/lists/mutable.py
@@ -1,7 +1,6 @@
 # A list contains multiple values in an ordered sequence
 #
-# Unlike strings, list are mutable
-# It can be changed
+# Unlike strings, list are mutable, they can be changes
 
 numbers = [1, 2]
 numbers[1] = 3
