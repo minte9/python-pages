66c269f89552d56338d2ad7c01ae3cdca359d9c1
minte9 <catalinprescure@gmail.com>
2021-11-22 16:29:05 +0200

    python-pages update

--- main/language/functions/destroyed.py
+++ main/language/functions/destroyed.py
@@ -1,11 +1,27 @@
 # A function local variable is destroyed ...
 # after the function is called
+#
+# If you need to modify a global variable from within a function ...
+# use global statement.
+
 
 def myfunc(a, b):
     c = a + b
     return c
 
-print(myfunc(3, 4)) # 7
+assert myfunc(3, 4) != None
+assert myfunc(3, 4) == 7
 
-# print(c) 
-    # NameError: name 'c' is not defined
+# print(c) # NameError: name 'c' is not defined
+
+
+n = 0
+def parse():
+    global n
+    for i in range(10):
+        n = i
+
+parse()
+
+assert n != 0
+assert n == 9
