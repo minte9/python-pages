8d570672bd90201e5c62255aa87f4415bad930c3
minte9 <catalinprescure@gmail.com>
2021-11-22 16:14:24 +0200

    python-pages update

--- main/language/functions/definition.py
+++ main/language/functions/definition.py
@@ -9,14 +9,13 @@
 def myfunc(a): 
     return a%2
 
-print (myfunc(3))
+print (myfunc(3)) # 1
 
 
 # In Python, there is value called None
 # which represents the absence of a value
 
 def myprint(x):
-    print(x)
     return
 
-assert myprint("2") == None # pass
+print (myprint("2")) # None
