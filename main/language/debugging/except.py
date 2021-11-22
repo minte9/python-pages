2d3ea524b079a40cbbca003408e9be4a0068e6f8
minte9 <catalinprescure@gmail.com>
2021-11-22 16:40:46 +0200

    python-pages update

---
+++ main/language/debugging/except.py
@@ -0,0 +1,14 @@
+# Errors can be handled with ...
+# try and except statements.
+
+def calc(number, divider):
+    try:
+        return number/divider
+    except:
+        print('Error: Invalid argument')
+        return
+
+
+assert calc(10, 2) == 5
+assert calc(10, 0) == None
+    # Error: Invalid argument
