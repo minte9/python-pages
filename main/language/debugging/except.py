6608ee361fb6c45452e80e0c8c5e0e913747917f
minte9 <catalinprescure@gmail.com>
2021-11-22 16:42:17 +0200

    python-pages update

--- main/language/debugging/except.py
+++ main/language/debugging/except.py
@@ -6,7 +6,7 @@ def calc(number, divider):
         return number/divider
     except:
         print('Error: Invalid argument')
-        return
 
 assert calc(10, 2) == 5
-assert calc(10, 0) == None  # Error: Invalid argument
+assert calc(10, 0) == None  # pass
+    # Error: Invalid argument
