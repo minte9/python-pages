abd9110495962550495ce93f73cecd5cd0c82e4a
minte9 <catalinprescure@gmail.com>
2021-11-22 16:55:36 +0200

    python-pages update

--- main/language/variables/variables.py
+++ main/language/variables/variables.py
@@ -1,26 +1,23 @@
 # An assignment creates a variable and gives it value.
 # If you give an illegal name of a variable, you get a syntax error.
 # An expression is a combination of values, variables, and operators.
-
+# You can assign multiple variables in on line
 
 # Assignment
 message = 'Hello World!'
 print(message)
     # Hello World!
 
-
 # Illegal name variables
 # name@ = "John" 
 # class = "myClass" 
      # SyntaxError: invalid syntax
-
 
 # Expression
 n = 17
 n = n + 24
 print(n)
     # 41
-
 
 # Multiple assignement
 a, b, c = [1, 2, 3]
