2c12ce33905c90212f31be8f6968ed00b2761afe
minte9 <catalinprescure@gmail.com>
2021-11-22 17:50:10 +0200

    python-pages update

--- main/language/functions/import.py
+++ main/language/functions/import.py
@@ -1,4 +1,8 @@
 # To use a function from a module, you have to import it.
+# 
+# When you save your scripts ...
+# take care not to use names used by Python modules.
+# such as math, sys, csv, etc
 
 import math
 
