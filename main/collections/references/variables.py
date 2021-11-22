3bccec7e82d6c49ed6edf75bf8ecf6a70a97f8bb
minte9 <catalinprescure@gmail.com>
2021-11-22 19:43:23 +0200

    python-pages update

---
+++ main/collections/references/variables.py
@@ -0,0 +1,21 @@
+# Tehnically, variables are storing references ...
+# to the computer memory locations where the values are stored
+
+# When you assign 42 to a variable,
+# you are creating 42 value in computer memory ...
+# and store a reference to it
+#
+# Integers are imutable
+a = 42
+
+# When you copy the value in a ....
+# you are copying the reference.
+b = a
+
+# When later you change the value of a, 
+# you are creating a new value and store a reference to it
+a = 100
+
+# This copy doesn't afect b
+assert a == 100
+assert b == 42
