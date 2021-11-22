07c484d67faacb8fa3b25ce167e59bc4758bb842
minte9 <catalinprescure@gmail.com>
2021-11-22 17:03:27 +0200

    python-pages update

--- main/collections/lists/concat.py
+++ main/collections/lists/concat.py
@@ -7,3 +7,12 @@ c = a + b
 assert c == [1, 2, 3, 4, 5, 6]
 assert c != [1, 2, 7, 5, 6]
 assert c != [1, 2, 3]
+
+# To multiply a list use * operator
+
+a = [2]
+b = [2] * 4
+
+assert b == [2, 2, 2, 2]
+assert b != 8
+assert b != [ [2], [2], [2], [2] ]
--- main/collections/lists/multiply.py
+++
@@ -1,8 +0,0 @@
-# Tu multiply a list use * operator
-
-a = [2]
-b = [2] * 4
-
-assert b == [2, 2, 2, 2]
-assert b != 8
-assert b != [ [2], [2], [2], [2] ]
