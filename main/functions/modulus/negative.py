c0b7a365947cf2f0252401ed79429b8b6f8b1b92
minte9 <catalinprescure@gmail.com>
2021-12-01 11:25:43 +0200

    python-pages update

--- main/functions/modulus/negative.py
+++ main/functions/modulus/negative.py
@@ -15,17 +15,17 @@ assert 23 // 5  ==  4
 assert 23 == 4*5 + 3  # reminder 3
 assert 23 % 5 ==  3
 
-# 23 % -5 
+# 23 % -5 = ?
 assert 23 // -5 == -5
 assert 23 == -5 * -5 - 2  # reminder -2
 assert 23 % -5 == -2 
 
-# -3 % 7 
+# -3 % 7 = ?
 assert -3 // 7 == -1
 assert -3 == -1*7 + 4  # remainder 4
 assert -3 % 7 == 4
 
-# distributive law
+# Distributive law
 assert (2 + 2) % 3 == 1
 assert (2 + 2) % 3 == (2 % 3 + 2 % 3) % 3
 assert (2 + 2) % 3 != 2 % 3 + 2 % 3
