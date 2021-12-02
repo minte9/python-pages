3c5e237f913f2d18b62ecf8992e43d4a1a61622b
minte9 <catalinprescure@gmail.com>
2021-12-02 16:17:15 +0200

    python-pages update

--- main/collections/coin_flip/coinflip_v2.py
+++ main/collections/coin_flip/coinflip_v2.py
@@ -1,39 +1,31 @@
 """Coin Flip:
 A program to find out how often
 a line of six heads or six tails comes up 
-in a randomly generated list of heads and tails."""
+in a randomly generated list of heads and tails.
+Version 2"""
 
 import random
 streaks = 0
 
-for i in range(10000): # Run experiment multiple times
-    HT = [] # Create a lists of 100 heads or tails (flips)
-
-    for j in range(100):
-        if random.randint(0,1) == 0:
-            HT.append('H')
-        else:
-            HT.append('T')
-    print(HT)
+for i in range(10000):
+    HT = random.choices('HT', k=100)
     
-    for k in range(len(HT)): # Search streaks of 6 heads or tails
-
+    for k in range(len(HT)):
+        
         count = 0
         for m in range(6):
             try:
                 if HT[k] == 'H' and HT[k] == HT[k+m]:
                     count += 1
                 else:
-                    count = 0
                     break
             except IndexError: # item 95
                 break
 
         if count == 6:
-            print('\x1b[6;30;42m' + str(HT[k:k+6]) + '\x1b[0m')
             streaks += 1
 
-posibilities = 95 * 10000 # maxim posibilities
+posibilities = 95 * 10000
 percentage = streaks * 100 / posibilities
 probability = percentage / 100
 
