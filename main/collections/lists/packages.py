9e5bc8d3719e3c78c7f55f731fa2935ccae3ae4a
minte9 <catalinprescure@gmail.com>
2021-12-03 15:23:41 +0200

    python-pages update

--- main/collections/lists/packages.py
+++ main/collections/lists/packages.py
@@ -3,7 +3,7 @@ From command line use: pip list"""
 import pkg_resources
 
 pkgs = pkg_resources.working_set
-pkgs = sorted(['%s \t %s' % (k.version, k.project_name) for k in pkgs])
+pkgs = sorted(['%s \t %s' % (k.version, k.key) for k in pkgs])
 
 print('\n'.join(pkgs))
     # ...
