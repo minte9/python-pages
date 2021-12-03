3ea2ccbc6b5231bcd48be26e2bd29f739e473524
minte9 <catalinprescure@gmail.com>
2021-12-03 15:18:11 +0200

    python-pages update

--- main/collections/lists/packages.py
+++ main/collections/lists/packages.py
@@ -4,9 +4,14 @@ From command line use: pip list
 import pkg_resources
 
 pkgs = pkg_resources.working_set
-pkgs = sorted([
-    '%s \t %s' % (k.version, k.project_name)
-    for k in pkgs
-])
+pkgs = sorted(['%s \t %s' % (k.version, k.project_name) for k in pkgs])
 
 print('\n'.join(pkgs))
+    # ...
+    # 3.0.4    chardet
+    # 3.1.0    oauthlib
+    # 3.1.7    bcrypt
+    # 3.12.0   louis
+    # 3.16.0   simplejson
+    # 3.36.0   PyGObject
+    # ...
