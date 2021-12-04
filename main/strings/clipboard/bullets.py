687b8bb1ad062559ead554b09abe4265dd6ed8a4
minte9 <catalinprescure@gmail.com>
2021-12-04 14:02:24 +0200

    python-pages update

---
+++ main/strings/clipboard/bullets.py
@@ -0,0 +1,24 @@
+"""Add bullets to a list
+The tranformed list will be automatically ...
+available in the clipboard.
+Copy:
+    Name: pyperclip
+    Version: 1.8.2
+    Author: Al Sweigart
+    License: BSD
+Run:
+    $ python bullets.py
+Paste:
+    * Name: pyperclip
+    * Version: 1.8.2
+    * Author: Al Sweigart
+    * License: BSD
+"""
+import pyperclip
+
+text = pyperclip.paste()
+lines = text.split('\n')
+lines = [f'* {k}' for k in lines]
+text = '\n'.join(lines)
+
+pyperclip.copy(text) # text in clipboard ready to be paste
