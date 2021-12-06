3101fc68e1be6ddce27c029c097cf40cf9860d6d
minte9 <catalinprescure@gmail.com>
2021-12-06 13:38:22 +0200

    python-pages update

---
+++ main/strings/regex/find_authors.py
@@ -0,0 +1,17 @@
+"""Find all authors @nickname in an amazon page.
+Page example: https://www.amazon.com/gp/product/1593279922
+Ctrl-A, Ctrl-C, run program, Ctrl-V
+"""
+import re, pyperclip
+
+clipboard = pyperclip.paste()
+
+pattern = re.compile('@[a-zA-Z0-9_-]+')
+authors = pattern.findall(clipboard)
+text = '\n'.join(authors)
+
+pyperclip.copy(text)
+print(pyperclip.paste())
+    # @OscarBaruffa
+    # @Awful_Curious
+    # @mcapablanca
