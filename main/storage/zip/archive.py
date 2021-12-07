827361cf7d4c00f6080afd3c82a09cff79a06027
minte9 <catalinprescure@gmail.com>
2021-12-07 18:01:22 +0200

    python-pages update

--- main/storage/zip/archive.py
+++ main/storage/zip/archive.py
@@ -5,8 +5,8 @@ import zipfile, os
 DIR = os.path.dirname(os.path.realpath(__file__))
 
 newZip = zipfile.ZipFile(DIR + '/data/archive.zip', 'w')
-newZip.write(DIR + '/data/file1.txt', 'file1', compress_type=zipfile.ZIP_DEFLATED)
-newZip.write(DIR + '/data/file2.txt', 'file2', compress_type=zipfile.ZIP_DEFLATED)
+newZip.write(DIR + '/data/file1.txt', 'file1')
+newZip.write(DIR + '/data/file2.txt', 'file2')
 newZip.close()
 
 archive = zipfile.ZipFile(DIR + '/data/archive.zip')
