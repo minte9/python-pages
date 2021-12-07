82dd8175522007ae8e5c7bc2dbbcc43812df87ad
minte9 <catalinprescure@gmail.com>
2021-12-07 18:00:08 +0200

    python-pages update

--- main/storage/zip/archive.py
+++ main/storage/zip/archive.py
@@ -1,17 +1,16 @@
 """Zip arhive
 Compressing multiple files into a single archive file.
 """
-import zipfile
-from pathlib import Path
+import zipfile, os
 
-DIR = Path(__file__).resolve().parent
+DIR = os.path.dirname(os.path.realpath(__file__))
 
-newZip = zipfile.ZipFile(DIR / 'data/archive.zip', 'w')
-newZip.write(DIR / 'data/file1.txt', 'file1', compress_type=zipfile.ZIP_DEFLATED)
-newZip.write(DIR / 'data/file2.txt', 'file2', compress_type=zipfile.ZIP_DEFLATED)
+newZip = zipfile.ZipFile(DIR + '/data/archive.zip', 'w')
+newZip.write(DIR + '/data/file1.txt', 'file1', compress_type=zipfile.ZIP_DEFLATED)
+newZip.write(DIR + '/data/file2.txt', 'file2', compress_type=zipfile.ZIP_DEFLATED)
 newZip.close()
 
-archive = zipfile.ZipFile(DIR / 'data/archive.zip')
+archive = zipfile.ZipFile(DIR + '/data/archive.zip')
 print(archive.namelist())
     # file1, file2
 
