398e84ab0a5b5de70d308eddb35bd96432e77714
minte9 <catalinprescure@gmail.com>
2021-12-09 14:21:31 +0200

    python-pages update

--- main/storage/csv/writer.py
+++ main/storage/csv/writer.py
@@ -5,12 +5,14 @@ On windows you'll also need to pass newline='' argument.
 import csv, pathlib
 DIR = pathlib.Path(__file__).resolve().parent
 
+# Writer
 with open(DIR / 'data/file2.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow([11, 12, 13])
     writer.writerow([21, 22, 23])
     writer.writerow([31, 32, 33])
 
+# Check
 with open(DIR / 'data/file2.csv') as file:
     reader = csv.reader(file)
     data = list(reader)
