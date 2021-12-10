7db48709ac6fa6426c3a6eb1581a309b7a7afb90
minte9 <catalinprescure@gmail.com>
2021-12-10 14:46:56 +0200

    python-pages update

--- main/scheduler/subprocess/arguments.py
+++ main/scheduler/subprocess/arguments.py
@@ -1,19 +1,21 @@
 """Open subprocess arguments:
 Most GUI applications will accept a single argument for a file to be open.
 You pass a list as sole argument to Popen()
-
-Each operating system has a program that is equivalent to double-click
-Windows: start / Linux: see
-
-The wait() method will block until the launched process has terminated.
-This is useful to let the user finish with other programs.
 """
 import subprocess, pathlib, sys
-
 DIR = pathlib.Path(__file__).resolve().parent
+
+
+"""The wait() method will block until the launched process has terminated.
+This is useful to let the user finish with other programs.
+"""
 A = subprocess.Popen(['/usr/bin/gedit', DIR/'files/A.txt'])
 A.wait()
 
+
+"""Each operating system has a program that is equivalent to double-click
+Windows: start / Linux: see
+"""
 with open(DIR / 'files/B.txt', 'w') as f:
     f.write('Hello World!')
 B = subprocess.Popen(['/usr/bin/see', DIR / 'files/B.txt'])
