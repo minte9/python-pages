53443a549637bc7d0cc4cd0d17790f3adfd634e8
minte9 <catalinprescure@gmail.com>
2021-12-15 13:38:11 +0200

    python-pages update

--- main/strings/config/parser.py
+++ main/strings/config/parser.py
@@ -10,6 +10,11 @@ HOME_UNIX = config['HOME']['UNIX']
 HOME_WINDOWS = config['HOME']['WINDOWS']
 M9_URLs = config['M9']['URLS'].split(',')
 
-print(HOME_UNIX) # /home/catalin
-print(HOME_WINDOWS )# c:/Users/catalin
-print(M9_URLs[1].strip()) # https://www.minte9.com/java
+print(HOME_UNIX) 
+    # /home/catalin
+
+print(HOME_WINDOWS )
+    # c:/Users/catalin
+    
+print(M9_URLs[1].strip())
+    # https://www.minte9.com/java
