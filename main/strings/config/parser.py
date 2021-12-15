edf88a7fb8f5db2eb72b6213c0c8e385b6357a2b
minte9 <catalinprescure@gmail.com>
2021-12-15 13:33:15 +0200

    python-pages update

---
+++ main/strings/config/parser.py
@@ -0,0 +1,19 @@
+"""Read config.ini file:
+"""
+import configparser, pathlib
+DIR = pathlib.Path(__file__).resolve().parent
+
+config = configparser.ConfigParser()
+config.read(DIR/'config.ini')
+
+HOME_UNIX = config['HOME']['UNIX']
+HOME_WINDOWS = config['HOME']['WINDOWS']
+M9_URLs = config['M9']['URLS'].split(',')
+
+print(HOME_UNIX)
+print(HOME_WINDOWS)
+print(M9_URLs[1].strip())
+
+# /home/catalin
+# c:/Users/catalin
+# https://www.minte9.com/java
