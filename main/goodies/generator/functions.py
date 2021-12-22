3305520cd624abded83cb61ca05468b7d856ee8b
minte9 <catalinprescure@gmail.com>
2021-12-22 16:15:30 +0200

    python-pages update

--- main/goodies/generator/functions.py
+++ main/goodies/generator/functions.py
@@ -2,10 +2,12 @@
 # with functions like sum, max, min
 
 y = sum(range(5))
+    # 0 + 1 + 2 + 3 + 4
 assert y == 10
 assert y != 15
 
-y = sum(x**2 for x in range(5)) # generator
+y = sum(x**2 for x in range(5)) # list comprehension
+    # 0 + 2 + 4 + 8 + 16
 assert y == 30
 assert y != 10
 
@@ -13,6 +15,6 @@ y = max([1, 10, 2, 3])
 assert y == 10
 assert y != 3
 
-y = max(x/2 for x in [10, 50, 20, 30]) # generator
+y = max(x/2 for x in [10, 50, 20, 30])
 assert y == 25.0
 assert y != 15.0
