86f675a8b62e4a44be52d9679e5a953798668a3c
minte9 <catalinprescure@gmail.com>
2022-10-28 17:40:04 +0300

    python-pages update

--- main/language/debugging/except.py
+++ main/language/debugging/except.py
@@ -4,9 +4,11 @@
 def calc(number, divider):
     try:
         return number/divider
+    except ZeroDivisionError:
+        print('Error: Division by zero')
     except:
         print('Error: Invalid argument')
 
 assert calc(10, 2) == 5
 assert calc(10, 0) == None  # pass
-    # Error: Invalid argument
+    # Error: Division by zero
