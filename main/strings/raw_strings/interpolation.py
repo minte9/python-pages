c95d0214f077b05dff15a49a4dd9255506b2cfa8
minte9 <catalinprescure@gmail.com>
2022-10-28 18:09:06 +0300

    python-pages update

--- main/strings/raw_strings/interpolation.py
+++ main/strings/raw_strings/interpolation.py
@@ -8,3 +8,6 @@ message = 'My name is %s and I\'m %s old'
 
 print(message % (name, age))
     # My name is John and I'm 40 old
+
+print("I am %s and have %s dogs " % ("John", 2))
+    # I am John and have 2 dogs 
--- main/strings/raw_strings/raw.py
+++ main/strings/raw_strings/raw.py
@@ -4,5 +4,5 @@ A raw string ignores all escape characters"""
 print('That\'s my cat')
     # That's my cat
 
-print(r'[a-z\'"]')
-    # [a-z\'"]
+print(r'[a-z\'A-Z]')
+    # [a-z\'A-Z]
