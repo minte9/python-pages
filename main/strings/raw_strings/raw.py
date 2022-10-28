b8258ca564a860869b17997d471ff86584e2d952
minte9 <catalinprescure@gmail.com>
2022-10-28 17:58:30 +0300

    python-pages update

--- main/strings/raw_strings/raw.py
+++ main/strings/raw_strings/raw.py
@@ -1,8 +1,8 @@
 """Raw strings:
 A raw string ignores all escape characters"""
 
-print(r'That\'s my cat')
-    # That\'s my cat
-
 print('That\'s my cat')
     # That's my cat
+
+print(r'[a-z\'"]')
+    # [a-z\'"]
