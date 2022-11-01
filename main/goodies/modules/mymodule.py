f36a9a7b2d45e462268cd2d6142029ef5756f55b
minte9 <catalinprescure@gmail.com>
2022-11-01 20:00:49 +0200

    python-pages update

--- main/goodies/modules/mymodule.py
+++ main/goodies/modules/mymodule.py
@@ -1,7 +1,7 @@
 """Define a module:
 When you import a module, it defines new functions, but doesn't run them.
 If the program is running as a script, the test code runs.
-If module is imported the test code is skiped.
+If module is imported the test code is skipped.
 """
 
 DIR = "/var/www/"
