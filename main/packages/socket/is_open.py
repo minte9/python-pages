9f5d7c4854de8621dcf0f462bc2ab832d9aaf4b6
minte9 <catalinprescure@gmail.com>
2022-11-17 18:35:30 +0200

    python-pages update

--- main/packages/socket/is_open.py
+++ main/packages/socket/is_open.py
@@ -1,12 +1,14 @@
 """Using Python to check if remote port is open and accessible
+connect_ex() return an error indicator ...
+instead of raising an exception
 """
 import socket
 
 def is_open(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(1)
-        result = s.connect_ex((host, port))
-        if result == 0:
+        res = s.connect_ex((host, port))
+        if res == 0:
             print("open")
         else:
             print("not open")
