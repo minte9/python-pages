563f4e446306d5bcdde38b45706286fa31759809
minte9 <catalinprescure@gmail.com>
2022-11-21 19:01:25 +0200

    python-pages update

--- main/functions/reassignment/reassign.py
+++ main/functions/reassignment/reassign.py
@@ -1,17 +1,18 @@
-# In Python it is legal to reassign a variable.
-# An assignment can make two variables equal, but not permanent.
+"""In Python it is legal to reassign a variable
+An assignment can make two variables equal, but not permanent
+"""
 
 x = 5
 x = 7
 
-assert x == 7
-assert x != 5
+assert x == 7 # pass
+assert x != 5 # pass
 
 a = 1
 b = 1
 
-assert a == b
+assert a == b # pass
 
 a = 2
 
-assert a != b
+assert a != b # pass
