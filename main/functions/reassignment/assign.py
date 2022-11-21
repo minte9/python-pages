da7d49d1919d0b0414fb70495af825687b6445dd
minte9 <catalinprescure@gmail.com>
2022-11-21 18:56:40 +0200

    python-pages update

--- main/functions/reassignment/assign.py
+++ main/functions/reassignment/assign.py
@@ -1,11 +1,11 @@
-# Variable assignement ...
-#
-# A variable must be assign before use
+"""A variable must be assign before use
+"""
 
 y = 0
 y = y + 1
+assert y == 1 # pass
 
-assert y == 1
-
-# z = z + 1
-#    # NameError: name 'z' is not defined
+try:
+    z = z + 1
+except NameError:
+    print('Variable z not defined')
