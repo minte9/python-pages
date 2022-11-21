1e6810197ad2418c39a06786936c101ca485097d
minte9 <catalinprescure@gmail.com>
2022-11-21 20:57:18 +0200

    python-pages update

--- main/packages/socket/is_open.py
+++ main/packages/socket/is_open.py
@@ -6,13 +6,17 @@ instead of raising an exception
 import socket
 
 def is_open(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(1)
-        res = s.connect_ex((host, port))
-        if res == 0:
-            print("open")
-        else:
-            print("not open")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+    res = s.connect_ex((host, port))
+    if res == 0:
+        print("%s %s open" % (host, port))
+    else:
+        print("%s %s closed" % (host, port))
 
 is_open('python.org', 80)
 is_open('python.org', 8080)
+
+host = input('Host: ') # localhost
+port = input('Port: ') # 80
+is_open(host, int(port))
