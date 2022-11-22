7fe6a25f0b879e607bb933dd53b251fcd38e41c1
minte9 <catalinprescure@gmail.com>
2022-11-22 10:26:14 +0200

    python-pages update

--- main/strings/config/parser.py
+++ main/strings/config/parser.py
@@ -11,6 +11,6 @@ HOME_WINDOWS = config['HOME']['WINDOWS']
 M9_URLs = config['M9']['URLS'].split(',')
 DEBUG = config['APP']['DEBUG']
 
-assert HOME_UNIX == '/home/catalin'
-assert M9_URLs[1].strip() == 'https://www.minte9.com/java'
-assert DEBUG == 'False'
+assert HOME_UNIX == '/home/catalin'                         # pass
+assert DEBUG == 'False'                                     # pass
+assert M9_URLs[1].strip() == 'https://www.minte9.com/java'  # pass
