40ced7d8e395bee0104daed7a173456ee06b463c
minte9 <catalinprescure@gmail.com>
2022-11-24 22:26:43 +0200

    python-pages update

--- main/language/conditional/input.py
+++ main/language/conditional/input.py
@@ -1,7 +1,5 @@
 """ Input function waits for user input
 """
 
-name = input('What is your name?\n') 
-    # type: John
-print(name) 
-    # output: John
+name = input('What is your name?\n') # type: John
+print(name) # output: John
