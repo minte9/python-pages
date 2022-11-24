147ed7a31c01e01025c6c6b2e7666367572d68e9
minte9 <catalinprescure@gmail.com>
2022-11-24 21:02:01 +0200

    python-pages update

--- main/language/functions/definition.py
+++ main/language/functions/definition.py
@@ -1,28 +1,21 @@
-# A function is a named sequence of statements.
-#
-# A function definition specifies the name and the statements.
-# Expression in parantheses is called the argument
-#
-# To end the function, you have to enter an empty line.
-# Statements inside a function don't run until the function is called
-#
-# In Python, there is value called None ...
-# which represents the absence of a value
-#
-# You can split a singule instruction on multple lines with \
+""" A function is a named sequence of statements
 
+To end the function, you have to enter an empty line
+Statements inside a function don't run until the function is called
+
+In Python, there is value called None ...
+which represents the absence of a value
+
+Split a single instruction on multple lines with \
+"""
 
 def myfunc(a): 
     return a%2
 
-print (myfunc(3)) # 1
-
-
 def myprint(x):
     return
 
-print (myprint("2")) # None
-
-
+print(myfunc(3))    # 1
+print(myprint("2")) # None
 print("Hello " + \
-        "World") # Hello World
+        "World")    # Hello World
