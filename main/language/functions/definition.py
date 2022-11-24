a5efa6a154e314060ac8a9dce7cbabb3618760d1
minte9 <catalinprescure@gmail.com>
2022-11-24 21:03:32 +0200

    python-pages update

--- main/language/functions/definition.py
+++ main/language/functions/definition.py
@@ -1,6 +1,6 @@
 """ A function is a named sequence of statements
 To end the function, you have to enter an empty line
-In Python, there is value called None, absence of a value
+In Python, there is value called None, absence of a value   
 Split a single instruction on multple lines with \
 """
 
@@ -10,7 +10,7 @@ def myfunc(a):
 def myprint(x):
     return
 
-print(myfunc(3))    # 1
+print(myfunc(3)) # 1
 print(myprint("2")) # None
 print("Hello " + \
-        "World")    # Hello World
+    "World") # Hello World
