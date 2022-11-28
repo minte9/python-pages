ddd03d90fe0756f96a458960187a8ba9d4340d88
minte9 <catalinprescure@gmail.com>
2022-11-28 11:24:35 +0200

    python-pages update

--- main/functions/modulus/negative.py
+++ main/functions/modulus/negative.py
@@ -1,31 +1,25 @@
-"""Modulus with negative numbers:
+"""Modulus with negative numbers
 
-Python modulus (%) always return a number with ...
-the same sign as the denominator. 
+In Python (%) returns same sign as the denominator
+  23 = 4*5 + 3      # reminder 3
+  23 = -5* 5 - 2    # reminder -2
+  -3 = -1*7 + 4     # remainder 4
 
-Python applies the distribute law of Modulo operator:
-
+Python applies distribute law of Modulo
   (a + b) mod n = ((a mod n) + (b mod n)) mod n
+  (2 + 2) % 3 = (2%3 + 2%3) % 3 = 1
+"""
 
-Python always does floor division."""
+assert (2 + 2) % 3 == 1 # distribute law of Modulo
+assert (2 + 2) % 3 == (2%3 + 2%3) % 3 == 1
+assert (2 + 2) % 3 != 2%3 + 2%3
 
-
-# 23 % 5 = ?
-assert 23 // 5  ==  4
-assert 23 == 4*5 + 3  # reminder 3
-assert 23 % 5 ==  3
-
-# 23 % -5 = ?
-assert 23 // -5 == -5
-assert 23 == -5 * -5 - 2  # reminder -2
-assert 23 % -5 == -2 
-
-# -3 % 7 = ?
-assert -3 // 7 == -1
-assert -3 == -1*7 + 4  # remainder 4
+assert 23 % 5 ==  3     # reminder 3
+assert 23 % -5 == -2    # same sign as denominator
 assert -3 % 7 == 4
 
-# Distributive law
-assert (2 + 2) % 3 == 1
-assert (2 + 2) % 3 == (2 % 3 + 2 % 3) % 3
-assert (2 + 2) % 3 != 2 % 3 + 2 % 3
+assert 23 // 5  ==  4   # floor division
+assert 23 // -5 == -5
+assert -3 // 7 == -1
+
+print('Tests passed')
--- main/functions/modulus/positive.py
+++ main/functions/modulus/positive.py
@@ -1,17 +1,15 @@
-# Modulus operation: 
-# 
-# dividend % divisor = reminder
-#
-# Python always does floor division
+"""Modulus operation 
+Python always does floor division
+dividend % divisor = reminder
+"""
 
 assert 0 % 7 == 0
 assert 1 % 7 == 1
-assert 2 % 7 == 2
-assert 3 % 7 == 3  # 3 // 7 = 0 reminder 3
+assert 3 % 7 == 3  # 3//7 = 0 reminder 3
 assert 7 % 7 == 0
-
 assert 8 % 7 == 1
 assert 9 % 7 == 2
-
 assert 25 % 7 == 4
-assert 7 % 25 == 7
+assert 7 % 25 == 7 # 7//25 = 0 reminder 7
+
+print('Tests passed')
