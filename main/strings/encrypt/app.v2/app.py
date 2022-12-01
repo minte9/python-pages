b45f19cd8d161c74e60ba0c4dde9aafc31d8fec1
minte9 <catalinprescure@gmail.com>
2022-12-01 16:57:31 +0200

    python-pages update

--- main/strings/encrypt/app.v2/app.py
+++ main/strings/encrypt/app.v2/app.py
@@ -8,32 +8,27 @@ $ python decrypt master_pass
 
 import sys, rsa
 
-if len(sys.argv) < 2: # Check cmd arguments
+if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 
 action = sys.argv[1]
 
 if action == 'encrypt':
-    msg = sys.argv[2] # pass_12333
-
-    with open('./public.pem', 'rb') as p: # Read public key
+    msg = sys.argv[2]
+    with open('./public.pem', 'rb') as p:
         publicKey = rsa.PublicKey.load_pkcs1(p.read())
-
-        with open('./password.bin', 'wb') as f: # Write encrypted
+        with open('./password.bin', 'wb') as f:
             encrypted = rsa.encrypt(msg.encode(), publicKey)
             f.write(encrypted)
-            print(encrypted) # b'[\xd6z\x7fP@\x96\x91\xa5\x01\x82\r'
+            print(encrypted)
 
 if action == 'decrypt':
     master_pass = sys.argv[2]
-
-    if master_pass != 'abc_123': # Check master
+    if master_pass != 'master_pass':
         sys.exit('Wrong master password')
-
-    with open('./password.bin', "rb") as f: # Read encrypted
+    with open('./password.bin', "rb") as f:
         encrypted = f.read()
-    
-    with open('./restricted/private.pem', "rb") as p: # decryt, with private key
+    with open('./restricted/private.pem', "rb") as p:
         privateKey = rsa.PrivateKey.load_pkcs1(p.read())
         decrypted = rsa.decrypt(encrypted, privateKey).decode()
-        print(decrypted) # pass_12333
+        print(decrypted)
