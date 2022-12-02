fc0d67b9907da213d478ce771317d5e46a6169bd
minte9 <catalinprescure@gmail.com>
2022-12-02 14:11:31 +0200

    python-pages update

--- main/collections/references/garbage.py
+++ main/collections/references/garbage.py
@@ -3,16 +3,16 @@ Delete any values not being referred to by any variables
 Manual memory management in other programming languages ...
 is a common source of bugs.
 The getrefcount function returns the number of references
+including the reference created for the argument
 """
 
 import sys
 
 a = 'somevalue'
-assert sys.getrefcount(a) == 4
 assert sys.getrefcount('somevalue') == 4
 
 b = a; c = b
-assert sys.getrefcount(a) == 6
+assert sys.getrefcount('somevalue') == 6
 
 del a; del b; del c
 assert sys.getrefcount('somevalue') == 3 # Look Here
