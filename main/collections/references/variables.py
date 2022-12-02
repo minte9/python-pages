cfe73de27a37947e4b86f77952d0aa3d220f5e0e
minte9 <catalinprescure@gmail.com>
2022-12-02 13:45:35 +0200

    python-pages update

--- main/collections/references/variables.py
+++ main/collections/references/variables.py
@@ -1,12 +1,12 @@
 """Variables as references
-When you assign 42 to a variable, you are creating 42 value
+When you assign a value to a variable, you are creating that value
 in computer memory, and store a reference to it
+When you assign new value to a, reference to a doesn't affect b
 """
 
-a = 42; b = a
-a = 100 # new value, store a reference to it
+a = 42; b = a; a = 100
 
 assert a == 100
-assert b == 42 # reference to a doesn't afect b
+assert b == 42
 
 print('Tests passed')
