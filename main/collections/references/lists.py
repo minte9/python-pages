fed42a928a54c07ddd392e0eecdfe228149698df
minte9 <catalinprescure@gmail.com>
2022-12-02 13:48:59 +0200

    python-pages update

--- main/collections/references/lists.py
+++ main/collections/references/lists.py
@@ -1,15 +1,13 @@
-# References are different with lists ...
-# because lists are mutable
+""" References are different with lists
+Lists are mutable
+Even the code touches only A list ...
+both A and B are changed!
+Values stored in A and B both refer to the same list
+"""
 
-A = [1, 2, 3]
-B = A
+A = [1, 2, 3]; B = A; A[1] = 'x'
 
-A[1] = 'x'
+assert A == [1, 'x', 3]
+assert B == [1, 'x', 3]
 
-assert A == [1, 'x', 3] # pass
-assert B == [1, 'x', 3] # pass
-
-# Even the code touches only A list ...
-# both A and B have changed!
-#
-# Values stored in A and B both refer to the same list
+print('Test passed')
