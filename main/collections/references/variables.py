3bf586f66b57b608d70c5b766fcf9806709841bf
minte9 <catalinprescure@gmail.com>
2022-12-02 13:41:25 +0200

    python-pages update

--- main/collections/references/variables.py
+++ main/collections/references/variables.py
@@ -1,12 +1,10 @@
 """Variables as references
-
 When you assign 42 to a variable,
 you are creating 42 value in computer memory ...
 and store a reference to it
 """
 
 a = 42; b = a
-
 a = 100 # new value, store a reference to it
 
 assert a == 100
