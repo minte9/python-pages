6b171ae916dd5ce623ec6cbf7fb01f70719d16c5
minte9 <catalinprescure@gmail.com>
2022-12-16 16:26:36 +0200

    python-pages update

--- main/scheduler/multithreading/startweb.py
+++ main/scheduler/multithreading/startweb.py
@@ -1,10 +1,21 @@
 """
-Start server / Open URL
-Run from console / python startweb.py 
-Interrupt / Ctrl + C to kill process
+Start server & Open URL
+
+python startweb.py
+    # Ctrl+C to kill the process
+    # ps -aF | grep python
+    # kill -9 no
+    
+Alternatives
+    php -S localhost:8000
+    python3 -m http.server 8000 
+    http://localhost:8000/public_html
 """
-import sys, time, threading, webbrowser
+import os, pathlib, sys, time, threading, webbrowser
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+DIR = pathlib.Path(__file__).resolve().parent
+os.chdir(DIR)
 
 def start_server():
     httpd = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
