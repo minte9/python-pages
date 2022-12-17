8d7125dbd405b23a819496ce82293e3f47308f0a
minte9 <catalinprescure@gmail.com>
2022-12-17 13:23:34 +0200

    python-pages update

---
+++ main/packages/image/jpg2png.py
@@ -0,0 +1,14 @@
+"""
+Python Imaging Library (expansion of PIL) is the de facto 
+image processing package for Python language
+Convert jpg to png
+"""
+
+import os, pathlib
+from PIL import Image
+
+DIR = pathlib.Path(__file__).resolve().parent
+os.chdir(DIR)
+
+img = Image.open(DIR / "01.jpeg")
+img.save(DIR / "01.png")
