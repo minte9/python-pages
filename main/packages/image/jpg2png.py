118f1b23ffc161df57dbd6b8c74a044c7f9c72d7
minte9 <catalinprescure@gmail.com>
2022-12-17 15:56:06 +0200

    python-pages update

--- main/packages/image/jpg2png.py
+++ main/packages/image/jpg2png.py
@@ -14,9 +14,10 @@ a = Image.open("01.jpg")    # .jpg
 b = Image.open("02.jpeg")   # .jpeg
 
 a.save("03.png")            # Look Here
+
 c = Image.open("03.png")
 c.show()
 
-print(a.format) # JPEG
-print(b.format) # JPEG
-print(c.format) # PNG
+print(a.format)             # JPEG
+print(b.format)             # JPEG
+print(c.format)             # PNG
