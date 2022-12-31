bc23a063cf50a39c59866dc3ee85e3f1e491243e
minte9 <catalinprescure@gmail.com>
2022-12-31 19:16:55 +0200

    python-pages update

--- main/machine-learning/knn/knn2.py
+++ main/machine-learning/knn/knn2.py
@@ -7,9 +7,13 @@ from sklearn.neighbors import KNeighborsClassifier
 
 # Training set of data points with corresponding labels
 data = {
-'height': [3.91, 7.09, 10.48, 9.21, 7.95, 7.62, 7.95, 4.69, 7.50, 7.11, 4.15, 7.29, 8.49, 7.44, 7.86, 3.93, 4.40, 5.5, 8.10, 8.69], 
-'width':  [5.76, 7.69, 7.32, 7.20, 5.90, 7.51, 5.32, 6.19, 5.99, 7.02, 5.60, 8.38, 6.52, 7.89, 7.60, 6.12, 5.90, 4.5, 6.15, 5.82],
-'fruit':  ['Mandarin', 'Apple', 'Lemon', 'Lemon', 'Lemon', 'Apple', 'Mandarin', 'Mandarin', 'Lemon', 'Apple', 'Mandarin', 'Apple', 'Lemon', 'Apple', 'Apple', 'Apple', 'Mandarin', 'Lemon', 'Lemon', 'Lemon']
+'height': [3.91, 7.09, 10.48, 9.21, 7.95, 7.62, 7.95, 4.69, 7.50, 7.11, 4.15, 7.29, 
+8.49, 7.44, 7.86, 3.93, 4.40, 5.5, 8.10, 8.69], 
+'width':  [5.76, 7.69, 7.32, 7.20, 5.90, 7.51, 5.32, 6.19, 5.99, 7.02, 5.60, 8.38, 
+6.52, 7.89, 7.60, 6.12, 5.90, 4.5, 6.15, 5.82],
+'fruit':  ['Mandarin', 'Apple', 'Lemon', 'Lemon', 'Lemon', 'Apple', 'Mandarin', 
+'Mandarin', 'Lemon', 'Apple', 'Mandarin', 'Apple', 'Lemon', 'Apple', 'Apple', 
+'Apple', 'Mandarin', 'Lemon', 'Lemon', 'Lemon']
 } 
 
 # Transform dataset into a DataFrame
