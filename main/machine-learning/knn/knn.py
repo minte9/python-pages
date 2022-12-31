b528957722dc1cbc64400e0a117e09962808182a
minte9 <catalinprescure@gmail.com>
2022-12-31 19:38:50 +0200

    python-pages update

--- main/machine-learning/knn/knn.py
+++ main/machine-learning/knn/knn.py
@@ -1,19 +1,14 @@
 """KNN is a simple, supervised machine learning (ML) algorithm
 that can be used for classification or regression tasks 
 """
-
 from sklearn.neighbors import KNeighborsClassifier
 
-# Training set of data points
+# Training set of data points and Labels
 X = [[0,0], [1,1], [2,2], [3,3]]    
-
-# Labels
 y = [0, 1, 0, 1]
 
-# KNN classifier with K=3
+# Createa a KNN classifier with K=3
 knn = KNeighborsClassifier(n_neighbors=3)
-
-# Fit the classifier
 knn.fit(X, y)
 
 # Predict the label of a new data point                
