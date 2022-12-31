6083299c827e824398418813c96169c9ab49a821
minte9 <catalinprescure@gmail.com>
2022-12-31 19:31:03 +0200

    python-pages update

--- main/machine-learning/knn/knn2.py
+++ main/machine-learning/knn/knn2.py
@@ -5,7 +5,8 @@ of height and width of a fruit to a (predicted) fruit type
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 
-data = { # Training set of data points with corresponding labels
+# Training set of data points with corresponding labels
+data = {
   'height': [
     3.91, 7.09, 10.48, 9.21, 7.95, 7.62, 7.95, 4.69, 7.50, 7.11, 
     4.15, 7.29, 8.49, 7.44, 7.86, 3.93, 4.40, 5.5, 8.10, 8.69
@@ -37,8 +38,10 @@ knn.fit(X, y)
 
 # Predict the label for new data point
 prediction = knn.predict([[9, 3]])
-print(prediction) # ['Lemon']
+print(prediction)  
+        # ['Lemon']
 
 # Predict multiple labels
 predictions = knn.predict([[9, 3], [4, 5], [2, 5], [8, 9], [5, 7]])
-print(predictions) # ['Lemon' 'Mandarin' 'Mandarin' 'Apple' 'Mandarin']
+print(predictions) 
+        # ['Lemon' 'Mandarin' 'Mandarin' 'Apple' 'Mandarin']
