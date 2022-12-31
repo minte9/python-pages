2dedeadbc059cab95871826f783f703797f2e2c9
minte9 <catalinprescure@gmail.com>
2022-12-31 19:03:29 +0200

    python-pages update

--- main/machine-learning/knn/knn.py
+++ main/machine-learning/knn/knn.py
@@ -1,5 +1,6 @@
-"""KNN is a simple, supervised machine learning (ML) algorithm 
-It can be used for classification or regression tasks 
+"""KNN
+Supervised machine learning (ML) algorithm ...
+that can be used for classification or regression tasks 
 pip install -U scikit-learn
 """
 
