c5320678fee80bfbe5a0b0f605cd8ec70758f6ed
minte9 <catalinprescure@gmail.com>
2023-01-02 19:09:53 +0200

    python-pages update

--- main/machine-learning/knn/knn.py
+++ main/machine-learning/knn/knn.py
@@ -1,5 +1,6 @@
 """ KNN Supervised machine learning (ML) algorithm
 K is the number of nearest neighbors to use
+
 Provide Training set of data points and Labels
 Createa a KNN classifier with K=3
 Predict the label of a new data point   
