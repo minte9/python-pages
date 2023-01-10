dbd56023dd3fc61a6148ea257e3247f8280caefd
minte9 <catalinprescure@gmail.com>
2023-01-10 14:56:24 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression2_two.py
+++ main/mlearning/linear_regression/linear_regression2_two.py
@@ -1,4 +1,4 @@
-""" Linear Regression (multiple parameters)
+""" Linear Regression (two parameters)
 We can predict the CO2 emission of a car based on the size of the engine, 
 but with multiple regression we can throw in more variables, 
 like the weight of the car, to make the prediction more accurate
