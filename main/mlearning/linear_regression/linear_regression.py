77e35228e6897dd002f8138c551a3f88f84748d3
minte9 <catalinprescure@gmail.com>
2023-01-12 15:35:01 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression.py
+++ main/mlearning/linear_regression/linear_regression.py
@@ -12,12 +12,6 @@ from sklearn.linear_model import LinearRegression
 x = np.array([[30], [46], [60], [65], [77], [95]])
 y = np.array([[31], [30], [80], [49], [70], [118]])
 
-fig, ax = plt.subplots()
-ax.plot(x, y, 'x', color='g', label='training data') # dataset points
-plt.ylim(0, 140)
-plt.xlim(0, 140)
-plt.legend() #, plt.show()
-
 # Learning a prediction function
 r = LinearRegression().fit(x, y)
 a = r.coef_[0][0].round(1)
@@ -25,14 +19,16 @@ b = r.intercept_[0].round(1)
 print(a) # 1.3
 print(b) # -18
 
-ax.plot(x, a*x + b, label=f'h(x) = {b} + {a}x') # function line
-plt.legend() #, plt.show()
-
 # Predict known
 P = []
 for i in x: # training dataset
     P.append(-18 + 1.3*i)
 print(f'Predictions: {P}')  # 21, 41.8, 60, ... 
+
+# Predict unknown
+xu = 80
+yu = -18 + 1.3*xu
+print(f'xu = {xu} -> yu = {yu}') # xu = 80 -> yu = 86.0
 
 # Evaluate the model
 R = [] 
@@ -45,15 +41,15 @@ for i in R:
     ssr += i**2 # sum of squared residuals
 print(f'SSR: {ssr}') # 1248.15
 
-for i in range(len(x)):
-    ax.plot([x[i], x[i]], [P[i], y[i]], '-', color='c') # residuals
+# Draw graphics
+fig, ax = plt.subplots()
+plt.ylim(0, 140)
+plt.xlim(0, 140)
 
-# Predict unknown
-x = 80
-y = -18 + 1.3*x
-print(f'x = {x} -> y = {y}') # x = 80 -> y = 86.0
+ax.plot(x, y, 'x', color='g', label='training data')        # dataset points
+ax.plot(x, a*x + b, label=f'h(x) = {b} + {a}x')             # function line
+ax.plot(xu, yu, 'o', color='r', label=f'h({xu}) = {yu}')    # prediction unknown
+for i in range(len(x)):                                     # residuals
+    ax.plot([x[i], x[i]], [P[i], y[i]], '-', color='c')
 
-ax.plot(x, y, 'o', color='r', label=f'h({x}) = {y}') # prediction
-
-# Show graphics
 plt.legend(), plt.show()
