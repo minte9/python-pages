dfeac9b6700ccd17984f08cb87ec468062fcd74f
minte9 <catalinprescure@gmail.com>
2023-01-12 17:36:31 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression3_multiple.py
+++ main/mlearning/linear_regression/linear_regression3_multiple.py
@@ -25,12 +25,10 @@ with open(DIR / 'data/real_estate.csv') as file:
 # Predict some train data
 A = [2013.17, 13, 732.85, 0, 24.98, 121.53]     # price: 39
 B = [2013.58, 16.6, 323.69, 6, 24.98, 121.54]   # price: 51
-a = r.predict([A]).round(1)
-b = r.predict([B]).round(1)
-print(a) # 38.8
-print(b) # 48.5
+print(r.predict([A]).round(1)) # 38.8
+print(r.predict([B]).round(1)) # 48.5
 
-# Predict unknown (like A, but house_age change from 13 to 33)
-X = [2013.17, 33, 732.85, 0, 24.98, 121.53]
-y = r.predict([X]).round(1)
-print(y) # 33.4
+# Predict unknown 
+# like A, but house_age change from 13 to 33)
+X = [2013.17, 33, 732.85, 0, 24.98, 121.53] # ?
+print(r.predict([X]).round(1)) # 33.4
