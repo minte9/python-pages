33cf27ef65caa3b909ecb106d4cb55a141748ebd
minte9 <catalinprescure@gmail.com>
2023-01-12 16:12:13 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression.py
+++ main/mlearning/linear_regression/linear_regression.py
@@ -12,44 +12,43 @@ from sklearn.linear_model import LinearRegression
 x = np.array([[30], [46], [60], [65], [77], [95]])
 y = np.array([[31], [30], [80], [49], [70], [118]])
 
-# Learning a prediction function
+# Learn a prediction function
 r = LinearRegression().fit(x, y)
 a = r.coef_[0][0].round(1)
 b = r.intercept_[0].round(1)
-print(a) # 1.3
-print(b) # -18
 
-# Predict known
-P = []
-for i in x: # training dataset
-    P.append(-18 + 1.3*i)
-print(f'Predictions: {P}')  # 21, 41.8, 60, ... 
+# Function
+print(f'f(x) = {a}x + {b}') # f(x) = 1.3x - 18
 
 # Predict unknown
-xu = 80
-yu = -18 + 1.3*xu
-print(f'xu = {xu} -> yu = {yu}') # xu = 80 -> yu = 86.0
+print(f'f({80}) = {-18 + 1.3*80}') # f(80) = 86.0
 
 # Evaluate the model
-R = [] 
+P = [] 
+R = []
+for i in x:
+    P.append(-18 + 1.3*i) # predictions
 for i in range(len(x)):
-    R.append(y[i] - P[i])
-print(f'Residuals: {R}') # 10, -11.8, 20, ...
+    R.append(y[i] - P[i]) # residuals
 
 ssr = 0
 for i in R:
     ssr += i**2 # sum of squared residuals
-print(f'SSR: {ssr}') # 1248.15
+
+print(f'Predictions: {P}')  # 21, 41.8, 60, ... 
+print(f'Residuals: {R}')    # 10, -11.8, 20, ...
+print(f'SSR: {ssr}')        # 1248.15
 
 # Draw graphics
 fig, ax = plt.subplots()
 plt.ylim(0, 140)
 plt.xlim(0, 140)
 
-ax.plot(x, y, 'x', color='g', label='training data')        # dataset points
-ax.plot(x, a*x + b, label=f'h(x) = {b} + {a}x')             # function line
-ax.plot(xu, yu, 'o', color='r', label=f'h({xu}) = {yu}')    # prediction unknown
-for i in range(len(x)):                                     # residuals
-    ax.plot([x[i], x[i]], [P[i], y[i]], '-', color='c')
+ax.plot(x, y, 'x', color='g', label='training data')            # dataset points
+ax.plot(x, a*x + b, label=f'h(x) = {b} + {a}x')                 # function line
+ax.plot(80, -18 + 1.3*80, 'o', color='r', label='h(80) = 86')   # prediction unknown
+
+for i in range(len(x)):
+    ax.plot([x[i], x[i]], [P[i], y[i]], '-', color='c') # residuals
 
 plt.legend(), plt.show()
