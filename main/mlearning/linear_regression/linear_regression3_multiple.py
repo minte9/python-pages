1c38cc4f4910b053f7b2044bff81304ef2eee826
minte9 <catalinprescure@gmail.com>
2023-01-12 19:27:23 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression3_multiple.py
+++ main/mlearning/linear_regression/linear_regression3_multiple.py
@@ -23,12 +23,13 @@ with open(DIR / 'data/real_estate.csv') as file:
     y = df['Y house price of unit area'].values
     r = LinearRegression().fit(X, y)
 
-# Predict some train data
-A = [2013.17, 13, 732.85, 0, 24.98, 121.53]     # price: 39
-B = [2013.58, 16.6, 323.69, 6, 24.98, 121.54]   # price: 51
-print(r.predict([A]).round(1)) # 38.8
-print(r.predict([B]).round(1)) # 48.5
-
-# Predict unknown (A alike, but house_age change from 13 to 33)
-X = [2013.17, 33, 732.85, 0, 24.98, 121.53] # ?
-print(r.predict([X]).round(1)) # 33.4
+# Predictions
+for X in [
+        [2013.17, 13, 732.85, 0, 24.98, 121.53],     # price: 39 (train data)
+        [2013.58, 16.6, 323.69, 6, 24.98, 121.54],   # price: 51 (train data)
+        [2013.17, 33, 732.85, 0, 24.98, 121.53],     # ?
+    ]:
+    print(r.predict([X]).round(1).item())
+        # 38.8
+        # 48.5
+        # 33.4
