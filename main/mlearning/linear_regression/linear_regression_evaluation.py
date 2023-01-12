a27c6d1fd0bc7b0d389e61570c780f9db2c61e5c
minte9 <catalinprescure@gmail.com>
2023-01-12 19:43:41 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression_evaluation.py
+++ main/mlearning/linear_regression/linear_regression_evaluation.py
@@ -15,7 +15,6 @@ y = np.array([[31], [30], [80], [49], [70], [118]])
 r = LinearRegression().fit(x, y)
 a = r.coef_[0][0].round(1)
 b = r.intercept_[0].round(1)
-print(f'f(x) = {a}x + {b}') # f(x) = 1.3x - 18
 
 # Evaluate the model
 P = [] # predictions (on training dataset)
@@ -26,9 +25,22 @@ for i in range(len(x)):
     P.append(-18 + 1.3*x[i])
     R.append(y[i] - P[i])
     SSR += R[i] ** 2
-    print(f'x = {x[i]} => y {P[i]} r = {R[i]}')
+    print(
+        f'x = {x[i].round(1).item()} \t' +
+        f'y = {P[i].round(1).item()} \t' +
+        f'r = {R[i].round(1).item()}'
+    )
+    """
+    x = 30      y = 21.0        r = 10.0
+    x = 46      y = 41.8        r = -11.8
+    x = 60      y = 60.0        r = 20.0
+    x = 65      y = 66.5        r = -17.5
+    x = 77      y = 82.1        r = -12.1
+    x = 95      y = 105.5       r = 12.5
+    """
 
-print(f'SSR: {SSR}') # 1248.15
+print(f'f(x) = {a}x + {b}')             # f(x) = 1.3x - 18
+print(f'SSR = {SSR.round(2).item()}')   # 1248.15
 
 # Draw graphics
 fig, ax = plt.subplots()
