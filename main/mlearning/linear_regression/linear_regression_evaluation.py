2eb6f465b5ddd3677137ac99c75373ee09420c8f
minte9 <catalinprescure@gmail.com>
2023-01-12 16:33:40 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression_evaluation.py
+++ main/mlearning/linear_regression/linear_regression_evaluation.py
@@ -1,7 +1,7 @@
 """ Linear Regression (one parameter)
 h(x) = ax + b
-Finding the line that best fits the data is known as ...
-linear regression (one of the most popular tools in statistics)
+Residuals, difference between the actual data points ...
+and the predicted (by our model) values
 """
 
 import numpy as np
@@ -25,6 +25,21 @@ x1 = 80
 y1 = a*x1 + b
 print(f'f({x1}) = {y1}') # f(80) = 86.0
 
+# Evaluate the model
+P = []
+R = []
+SSR = 0
+for i in x:
+    P.append(-18 + 1.3*i) # predictions (on training dataset)
+for i in range(len(x)):
+    R.append(y[i] - P[i]) # residuals
+for i in R:
+    SSR += i**2 # sum of squared residuals
+
+print(f'Predictions: {P}')  # 21, 41.8, 60, ... 
+print(f'Residuals: {R}')    # 10, -11.8, 20, ...
+print(f'SSR: {SSR}')        # 1248.15
+
 # Draw graphics
 fig, ax = plt.subplots()
 plt.ylim(0, 140)
@@ -33,5 +48,8 @@ plt.xlim(0, 140)
 ax.plot(x, y, 'x', color='g', label='training data')     # dataset points
 ax.plot(x, a*x + b, label=f'h(x) = {b} + {a}x')          # function line
 ax.plot(x1, y1, 'o', color='r', label=f'h({x1}) = {y1}') # prediction unknown
+
+for i in range(len(x)):
+    ax.plot([x[i], x[i]], [P[i], y[i]], '-', color='c') # residuals
 
 plt.legend(), plt.show()
