96b63678d7952688ce8c4acb387ca4da8872a221
minte9 <catalinprescure@gmail.com>
2023-01-13 12:52:03 +0200

    python-pages update

---
+++ main/packages/watchdog/file_changed_or_added.py
@@ -0,0 +1,37 @@
+from asyncio import subprocess
+import time, sys
+from subprocess import Popen
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+import pathlib
+
+DIR = pathlib.Path(__file__).resolve().parent
+DIR = str(DIR).replace('python-pages/main/packages/watchdog', '')
+DIR_TO_WATCH = DIR + 'python-pages/main/'
+
+class ScriptTriggerHandler(FileSystemEventHandler):
+
+    def on_modified(self, event):
+        Popen(['git', 'add ' + DIR_TO_WATCH], stdout=subprocess.PIPE)
+        Popen(['git', 'exportmdfcurl'])
+        print('File was modified')
+
+    def on_created(self, event):
+        Popen(['git', 'add ' + DIR_TO_WATCH], stdout=subprocess.PIPE)
+        Popen(['git', 'exportmdfcurl'])
+        print('File or directory was created')
+
+event_handler = ScriptTriggerHandler()
+observer = Observer()
+observer.schedule(event_handler, DIR_TO_WATCH, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
+
