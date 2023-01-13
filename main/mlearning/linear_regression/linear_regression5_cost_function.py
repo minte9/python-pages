e2f3dc49ff64ee261fcb7789ba5deb8f429d253c
minte9 <catalinprescure@gmail.com>
2023-01-13 09:59:50 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression5_cost_function.py
+++ main/mlearning/linear_regression/linear_regression5_cost_function.py
@@ -1,7 +1,7 @@
 """ Parameterized SSR (cost function) (p64)
 f(x) = ax + b
 How can we build an algorithm to find the best fit parameter for f(x)
-First, we need a way to measire the goodness-of-fit (SSR)
+First, we need a way to measure the goodness-of-fit (SSR)
 Second, search over all posible values of a and b (gradient descent)
 """
 
@@ -18,14 +18,8 @@ plt.xlim(0, 140)
 ax.plot(x, y, 'o', color='g', label='training data') # training points
 
 # Generate some a range values
-A = np.linspace(-2, 4.5, 13)
-for a in A:
-    print(a.round(), end=', ')
-    """
-        -2.0, -1.0, -1.0, -0.0, 0.0, 
-        1.0, 1.0, 2.0, 2.0, 3.0, 
-        3.0, 4.0, 4.0
-    """
+A = np.linspace(-2, 4.5, 13) # 21 values
+print(A)
 
 # Plot regression lines (for each a)
 for i in range(len(A)):
@@ -48,31 +42,18 @@ for a in A:
 
     SSR.append(np.sum(SR))
     print(f'a = {a.round(1)} SSR = {np.sum(SR).round()}')
-    """
-        a = -2.0 SSR = 282654.0
-        a = -1.5 SSR = 197923.0
-        a = -0.9 SSR = 128329.0
-        a = -0.4 SSR = 73872.0
-        a = 0.2  SSR = 34552.0
-        a = 0.7  SSR = 10368.0
-        a = 1.2  SSR = 1320.0
-        a = 1.8  SSR = 7409.0
-        a = 2.3  SSR = 28635.0
-        a = 2.9  SSR = 64998.0
-        a = 3.4  SSR = 116497.0
-        a = 4.0  SSR = 183133.0
-    """
+        # a = -2.0 SSR = 282654.0 ...
 
-# Define cost function J (SSR(a))
+# Generic cost function J (SSR(a))
 def J(a, b, x, y, m):
     J = 0
-    for i in range(m):
+    for i in range(m): # number of train points
         J += (y[i] - (a*x[i] + b))**2
     return J
 
-# Plot Cost function for coeficient a
+# Plot Cost function for coeficient (a)
 fig, ax = plt.subplots()
-ax.plot(A, J(A, -18, x, y, m=len(x))) # points
+ax.plot(A, J(A, -18, x, y, m=len(x))) # draw points
 for a in A:
    ax.plot(a, J(a, -18, x, y, m=len(x)), 'o', label='J(%.1f, -18)' %a) # line
 plt.legend() #; plt.show()
@@ -81,9 +62,9 @@ plt.legend() #; plt.show()
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1,projection='3d')
-a0 = np.linspace(-1,4,20)
-a1 = np.linspace(-100,100,10)
-aa0, aa1 = np.meshgrid(a0, a1)
-ax.plot_surface(aa0, aa1, J(aa0,aa1,x,y,m=len(x)))
+a = np.linspace(-1, 4, 20)
+b = np.linspace(-100, 100, 10)
+aa, bb = np.meshgrid(a, b)
+ax.plot_surface(aa, bb, J(aa, bb, x, y, m=len(x)))
 ax.view_init(50,-150)
 plt.show()
