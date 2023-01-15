12261e20695c84a370384ef0f0ede42badf9eb21
minte9 <catalinprescure@gmail.com>
2023-01-15 20:08:55 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression4_evaluation.py
+++ main/mlearning/linear_regression/linear_regression4_evaluation.py
@@ -8,36 +8,36 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 # Training Dataset
-x = np.array([[30], [46], [60], [65], [77], [95]])
-y = np.array([[31], [30], [80], [49], [70], [118]])
+X = np.array([[30], [46], [60], [65], [77], [95]])
+Y = np.array([31, 30, 80, 49, 70, 118])
 
 # Learn a prediction function
-r = LinearRegression().fit(x, y)
-a = r.coef_[0][0].round(1)
-b = r.intercept_[0].round(1)
+r = LinearRegression().fit(X, Y)
+a = r.coef_[0].round(1)
+b = r.intercept_.round(1)
+print(f'f(x) = {a}x + {b}') # f(x) = 1.3x - 18
 
 # Evaluate the model
-P = [] # predictions (on training dataset)
-R = [] # residuals  
+P = []  # predictions (on training dataset)
+R = []  # residuals  
 SSR = 0 # sum of squared residuals
 
-for i in range(len(x)):
-    P.append(-18 + 1.3*x[i])
-    R.append(y[i] - P[i])
+for i in range(len(X)):
+    P = np.append(P, -18 + 1.3*X[i])
+    R = np.append(R, Y[i] - P[i])
     SSR += R[i] ** 2
-    print(f'{x[i]} {P[i]} / {R[i]}')    # 30, 21 / 10 ...
 
-print(f'f(x) = {a}x + {b}')             # f(x) = 1.3x - 18
-print(f'SSR = {SSR.round(2).item()}')   # 1248.15
+print(R)                              # 10 -11.8 20 -17.5 -12.1 12.5
+print(f'SSR = {SSR.round(2).item()}') # 1248.15
 
 # Draw graphics
 fig, ax = plt.subplots()
 plt.ylim(0, 140)
 plt.xlim(0, 140)
 
-ax.plot(x, y, 'x', color='g', label='training data')     # dataset points
-ax.plot(x, a*x + b, label=f'h(x) = {b} + {a}x')          # function line
-for i in range(len(x)):                                  # residuals
-    ax.plot([x[i], x[i]], [P[i], y[i]], '-', color='c')
+ax.plot(X, Y, 'x', color='g', label='training data')     # dataset points
+ax.plot(X, a*X + b, label=f'h(x) = {b} + {a}x')          # function line
+for i in range(len(X)):                                  # residuals
+    ax.plot([X[i], X[i]], [P[i], Y[i]], '-', color='c')
 
 plt.legend(), plt.show()
