fafaa41c4cf8dffca4438808f198ea988e26134d
minte9 <catalinprescure@gmail.com>
2023-01-15 19:30:10 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression.py
+++ main/mlearning/linear_regression/linear_regression.py
@@ -9,13 +9,13 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 # Training Dataset
-x = np.array([[30], [46], [60], [65], [77], [95]])
-y = np.array([[31], [30], [80], [49], [70], [118]])
+X = np.array([[30], [46], [60], [65], [77], [95]])
+Y = np.array([31, 30, 80, 49, 70, 118])
 
 # Learn a prediction function
-r = LinearRegression().fit(x, y)
-a = r.coef_[0][0].round(1)
-b = r.intercept_[0].round(1)
+r = LinearRegression().fit(X, Y)
+a = r.coef_[0].round(1)
+b = r.intercept_.round(1)
 
 # Function learned
 print(f'f(x) = {a}x + {b}') # f(x) = 1.3x - 18
@@ -29,7 +29,7 @@ print(f'f({x1}) = {y1}') # f(80) = 86.0
 fig, ax = plt.subplots()
 plt.ylim(0, 140)
 plt.xlim(0, 140)
-ax.plot(x, y, 'x', color='g', label='training data')     # dataset points
-ax.plot(x, a*x + b, label=f'h(x) = {b} + {a}x')          # function line
+ax.plot(X, Y, 'x', color='g', label='training data')     # dataset points
+ax.plot(X, a*X + b, label=f'h(x) = {b} + {a}x')          # function line
 ax.plot(x1, y1, 'o', color='r', label=f'h({x1}) = {y1}') # prediction unknown
 plt.legend(), plt.show()
