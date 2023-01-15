1e734caeeb47fa17f8ea16ffae8dd5dc005621ee
minte9 <catalinprescure@gmail.com>
2023-01-15 20:55:03 +0200

    python-pages update

--- main/mlearning/linear_regression/linear_regression6_gradient_descent2.py
+++ main/mlearning/linear_regression/linear_regression6_gradient_descent2.py
@@ -15,12 +15,12 @@ def predict(X, a, b):
     return X * a + b # f(x) = ax + b
 
 # Cost function
-def J(a, b, X, Y):
-    J = sum((Y - predict(X, a, b)**2) for i in range(len(X)))
+def J(a, b):
+    J = np.sum(Y - predict(X, a, b)**2)
     return J
 
  # Derivatives
-def dJ(a, b, X, Y, m):
+def dJ(a, b):
     da = -2 * np.sum(X * (X - (a * X + b))) # b fixed
     db = -2 * np.sum(1 * (X - (a * X + b))) # a fixed
     return da, db
@@ -28,7 +28,7 @@ def dJ(a, b, X, Y, m):
 # Gradient descent
 def gradient_descent(a, b, lr=0.00001, loops=15):
     for i in range(loops):
-        da, db = dJ(a, b, X, Y, len(X))
+        da, db = dJ(a, b)
         a = a - lr * da
         b = b - lr * db 
     print('a =', round(a, 1), ' b =', round(b, 1))
@@ -36,7 +36,7 @@ def gradient_descent(a, b, lr=0.00001, loops=15):
 
 
 # Learning
-a, b = gradient_descent(0, 0)
+a, b = gradient_descent(0, -10)
 
 # Prediction
 print(95, round(predict(95, a, b)))
