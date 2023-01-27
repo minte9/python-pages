e1945934c8a6e0c434a1f1a0ccc75147e306ea76
minte9 <catalinprescure@gmail.com>
2023-01-27 13:29:35 +0200

    python-pages update

--- main/packages/virtualenv/venv.sh
+++ main/packages/virtualenv/venv.sh
@@ -1,23 +1,19 @@
 # Upgrade pip
 python -m pip install --upgrade pip
 
+
 # Install package
 pip install virtualenv
 
 # Create venv right into project folder 
 virtualenv ./my_env
 
+
 # Load and activate virtual environment 
 . my_env/bin/activate
 
 # Test environment
 pip list
-
-# Exit virtual environment
-deactivate
-
-# Repo ignore 
-echo 'my_env/' > .gitignore
 
 : '
 Package    Version
@@ -26,3 +22,10 @@ pip        22.3.1
 setuptools 65.6.3
 wheel      0.38.4
 '
+
+
+# Exit virtual environment
+deactivate
+
+# Repo ignore 
+echo 'my_env/' > .gitignore
