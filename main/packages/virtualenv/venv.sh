77f0cf62fce96099a2d19ad2d3daab49c109bc53
minte9 <catalinprescure@gmail.com>
2023-01-27 14:02:47 +0200

    python-pages update

--- main/packages/virtualenv/venv.sh
+++ main/packages/virtualenv/venv.sh
@@ -2,8 +2,10 @@ python -m pip install --upgrade pip
 pip install virtualenv
 
 virtualenv ./my_env     # create
+
 . my_env/bin/activate   # load and activate
-pip list                # test           
+pip list     
+
 deactivate              # exit  
 
 : '
@@ -13,6 +15,5 @@ pip        22.3.1
 setuptools 65.6.3
 wheel      0.38.4
 '
-
         
 echo 'my_env/' > .gitignore
