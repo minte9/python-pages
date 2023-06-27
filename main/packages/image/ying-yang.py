26e7402e4a0a4eece1fd12ada93f9fe4e6417d48
minte9 <catalinprescure@gmail.com>
2023-06-27 19:13:55 +0300

    python-pages update

--- main/packages/image/ying-yang.py
+++ main/packages/image/ying-yang.py
@@ -1,21 +1,21 @@
 from PIL import Image, ImageDraw
 
 # Create a new blank image
-image = Image.new("RGB", (400, 400), "white")
+image = Image.new("RGB", (500, 500), "white")
 
 # Create a drawing object
 draw = ImageDraw.Draw(image)
 
 # Draw the outer circle
-draw.ellipse((20, 20, 350, 350), fill=None, outline="black")
+draw.ellipse((20, 20, 380, 380), fill=None, outline="black")
 
 # Draw the inner circles
-draw.ellipse((150, 50, 250, 150), fill=None, outline="black")
-draw.ellipse((150, 250, 250, 350), fill=None, outline="black")
+draw.ellipse((150, 60, 250, 160), fill=None, outline="black")
+draw.ellipse((150, 240, 250, 340), fill=None, outline="black")
 
 # Draw the curved median lines
-draw.arc((100, 20, 280, 190), -90, 90, fill="black")
-draw.arc((100, 190, 280, 380), 90, 270, fill="black")
+draw.arc((100, 20, 290, 200), -90, 90, fill="black")
+draw.arc((100, 200, 290, 380), 90, 270, fill="black")
 
 # Display the image
 image.show()
