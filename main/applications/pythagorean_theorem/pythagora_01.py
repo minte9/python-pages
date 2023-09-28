532aa2d86b7dafb9c978d54a5d32c09a37619fa0
minte9 <catalinprescure@gmail.com>
2023-09-28 15:00:25 +0300

    python-pages update

---
+++ main/applications/pythagorean_theorem/pythagora_01.py
@@ -0,0 +1,8 @@
+""" Pythagorean Theorem - First step
+Define the function with wrong answer but syntactically correct
+"""
+
+def distance(x1, y1, x2, y2):
+    return 0.0
+
+assert distance(1, 2, 4, 6) == 0.0
