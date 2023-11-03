3c9d8d8e53aba46a9f79345576e1e9ae40586b65
minte9 <catalinprescure@gmail.com>
2023-11-03 14:25:21 +0200

    python-pages update

--- main/packages/flask/app.py
+++ main/packages/flask/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Docker v4!"
+    return "Hello, Docker v5!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
