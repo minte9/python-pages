9c40c513c05bdc885a02768e49d8255faff8e1b1
minte9 <catalinprescure@gmail.com>
2023-12-14 16:30:19 +0200

    python-pages update

---
+++ main/numpy/matrices/vectors.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+vectorA = np.array([1, 2, 3])
+vectorB = np.array([[1], [4], [3]])
+
+print("Row vector: \n", vectorA)
+print("Column vector: \n", vectorA)
+
+"""
+    Row vector: 
+     [1 2 3]
+
+    Column vector: 
+    [[1]
+     [4]
+     [3]]
+"""
