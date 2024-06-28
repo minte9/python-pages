0a267e7ca17550d46b252260252ed425a3b6a57e
minte9 <catalinprescure@gmail.com>
2024-06-28 17:00:26 +0300

    python-pages update

---
+++ main/goodies/list_comprehension/set_comprehension.py
@@ -0,0 +1,13 @@
+# Set comprehension looks like list comprehension 
+# except with curly braches instead of square brackets
+
+# List comprehension
+STRINGS = ['a', 'as', 'bat', 'car', 'dove', 'python']
+UPPERS = [x.upper() for x in STRINGS if len(x) > 2]
+print(UPPERS)
+    # ['BAT', 'CAR', 'DOVE', 'PYTHON']
+
+# Set comprehension
+UNIQUE_LENGTHS = {len(x) for x in STRINGS}
+print(UNIQUE_LENGTHS)
+    # {1, 2, 3, 4, 6}
