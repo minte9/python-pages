c5fb7a7aee6ec839b8f1a01b7b28db6b2aa9e9ad
minte9 <catalinprescure@gmail.com>
2024-06-28 15:43:17 +0300

    python-pages update

--- main/goodies/list_comprehension/list_comprehension.py
+++ main/goodies/list_comprehension/list_comprehension.py
@@ -6,13 +6,13 @@ D = []
 for s in str:
     D.append(s.capitalize())
 
-
 # With list comprehension / More concise but are harder to debug ...
 
 str = "abc"
 E = [s.capitalize() for s in str]
 
 # Output
+
 print(D) # ['A', 'B', 'C']
 print(E) # ['A', 'B', 'C']
 
