6343e8c3c4cedd4855a2910be87963c27d6eefc9
minte9 <catalinprescure@gmail.com>
2024-06-28 16:11:38 +0300

    python-pages update

--- main/goodies/list_comprehension/list_comprehension.py
+++ main/goodies/list_comprehension/list_comprehension.py
@@ -1,18 +1,19 @@
-# For Loop with list comprehension (more concise, but harder to debug)
-
+# Input string
 str = "abcd"
+
+# For Loop with list comprehension (more concise, but harder to debug)
 D = [s.capitalize() for s in str if s in ['a', 'b', 'c']]
 
 # For Loop without list comprehension (less concise, but easier to debug)
-
-str = "abcd"
 E = []
 for s in str:
     if s in ['a', 'b', 'c']:
         E.append(s.capitalize())
 
 
+# Output
 print(D) # ['A', 'B', 'C']
 print(E) # ['A', 'B', 'C']
 
+# Assertion
 assert D == E
