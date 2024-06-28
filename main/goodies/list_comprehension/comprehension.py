f2e9c16e882592cfd89e236a1ff9dee07c4481ef
minte9 <catalinprescure@gmail.com>
2024-06-28 15:36:06 +0300

    python-pages update

--- main/goodies/list_comprehension/comprehension.py
+++
@@ -1,9 +0,0 @@
-# List comprehension
-#
-# The loop variable appears in expression before we get to definition
-# List comprehensions are more concise but are harder to debug
-
-str = "abc"
-res = [s.capitalize() for s in str]
-
-assert res == ['A', 'B', 'C']
---
+++ main/goodies/list_comprehension/list_comprehension.py
@@ -0,0 +1,19 @@
+# Without list comprehension
+# Normal use of a for loop ...
+
+str = "abc"
+res = []
+
+for s in str:
+    res.append(s.capitalize())
+
+print(res) # ['A', 'B', 'C']
+
+
+# With list comprehension
+# More concise but are harder to debug ...
+
+str = "abc"
+res = [s.capitalize() for s in str]
+
+print(res) # ['A', 'B', 'C']
