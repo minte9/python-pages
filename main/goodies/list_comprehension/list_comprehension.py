1342bff39f0ee1b4ebf1d195d74f4ef5ab048c3e
minte9 <catalinprescure@gmail.com>
2024-06-28 16:02:40 +0300

    python-pages update

--- main/goodies/list_comprehension/list_comprehension.py
+++ main/goodies/list_comprehension/list_comprehension.py
@@ -1,11 +1,9 @@
-# For Loop - With list comprehension 
-# More concise but are harder to debug ...
+# For Loop - With list comprehension (more concise, but harder to debug) ...
 
 str = "abcd"
 D = [s.capitalize() for s in str if s in ['a', 'b', 'c']]
 
-# For Loop - Without list comprehension 
-# Normal use of a for loop ...
+# For Loop - Without list comprehension (less concise, but easier to debug) ...
 
 str = "abcd"
 E = []
