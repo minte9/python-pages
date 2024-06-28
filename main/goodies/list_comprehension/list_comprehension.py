5f6e625d54183e7458a10503e7a88adce810edd7
minte9 <catalinprescure@gmail.com>
2024-06-28 15:41:41 +0300

    python-pages update

--- main/goodies/list_comprehension/list_comprehension.py
+++ main/goodies/list_comprehension/list_comprehension.py
@@ -2,20 +2,20 @@
 # Normal use of a for loop ...
 
 str = "abc"
-C1 = []
+D = []
 
 for s in str:
-    C1.append(s.capitalize())
+    D.append(s.capitalize())
 
 
 # With list comprehension
 # More concise but are harder to debug ...
 
 str = "abc"
-C2 = [s.capitalize() for s in str]
+E = [s.capitalize() for s in str]
 
 # Output
-print(C1) # ['A', 'B', 'C']
-print(C2) # ['A', 'B', 'C']
+print(D) # ['A', 'B', 'C']
+print(E) # ['A', 'B', 'C']
 
-assert C1 == C2
+assert D == E
