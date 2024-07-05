3230d827e09991e278eb612689ea218f8c08e03e
minte9 <catalinprescure@gmail.com>
2024-07-05 12:03:16 +0300

    python-pages update

--- main/goodies/list_comprehension/set_comprehension.py
+++ main/goodies/list_comprehension/set_comprehension.py
@@ -1,5 +1,3 @@
-# Set comprehension uses curly braches instead of square brackets
-
 # Input list
 STRINGS = ['a', 'as', 'bat', 'car', 'dove', 'python']
 
@@ -8,7 +6,7 @@ UPPERS = [len(x) for x in STRINGS]
 print(UPPERS)
     # [1, 2, 3, 3, 4, 6]
 
-# Set comprehension
+# Set comprehension (curly braches instead of square brackets)
 UNIQUE_LENGTHS = {len(x) for x in STRINGS}
 print(UNIQUE_LENGTHS)
     # {1, 2, 3, 4, 6}
