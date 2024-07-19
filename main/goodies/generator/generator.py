9046ca87283b2b74b2aad7a339ae2c881deab256
minte9 <catalinprescure@gmail.com>
2024-07-19 15:58:26 +0300

    python-pages update

--- main/goodies/generator/generator.py
+++ main/goodies/generator/generator.py
@@ -3,13 +3,29 @@
     A generator can return a sequence of values by pausing and resuming execution.
 """
 
-def squares(n=10):
-    print("Generating squares from 1 to", n**2)
-    for i in range(1, n+1):
-        yield i**2
+# Functions return values
+def squares_func():
+    lst = []
+    for i in range(1, 10):
+        m = i**2
+        lst.append(m)
+    return lst
 
-mygen = squares()
-for x in mygen:
+print("\nLooping through the function returned value:")
+A = squares_func()
+for x in A:
     print(x, end=' ')
         # Generating squares from 1 to 100
         # 1 4 9 16 25 36 49 64 81 100
+
+
+# Generators yield values
+def squares_gen():
+    for i in range(1, 10):
+        yield i**2
+
+print("\nLooping using iter object:")
+G = squares_gen()
+itr = iter(G)
+for i in range(1, 3):
+    print(next(itr))
