60cf4eb4cd3bc0b5f36aa02a8be61383c46dc700
minte9 <catalinprescure@gmail.com>
2024-08-01 15:00:29 +0300

    python-pages update

--- main/goodies/list_comprehension/code/set_comprehension.py
+++ main/goodies/list_comprehension/code/set_comprehension.py
@@ -2,14 +2,9 @@
 S = ['a', 'as', 'bat', 'car', 'rac', 'dove', 'python']
 
 # List comprehension (square brackets)
-UPPERS = [len(x) for x in S]
-print(UPPERS)
+LENGTHS = [len(x) for x in S]
+print(LENGTHS)
 
 # Set comprehension (curly braches)
 UNIQUE_LENGTHS = {len(x) for x in S}
 print(UNIQUE_LENGTHS)
-
-"""
-    [1, 2, 3, 3, 3, 4, 6]
-    {1, 2, 3, 4, 6}
-"""
