98a5b55513a92163a9cbe5dc95702a86fd9e51ed
minte9 <catalinprescure@gmail.com>
2024-08-02 15:28:59 +0300

    python-pages update

--- main/goodies/conditional_expression/code/conditional_expression.py
+++ main/goodies/conditional_expression/code/conditional_expression.py
@@ -1,9 +1,10 @@
-# Conditional expression (concise)
+""" We can write if/else statements more concisely using conditional expression.
+"""
+
+# Conditional expression
 a = 0
 b = 100 if a >= 0 else -1
-
 print(b) # 100
-
 
 # Normal use of if/else
 a = 0
@@ -11,5 +12,4 @@ if a >= 0:
     b = 100
 else:
     b = -1
-
 print(b) # 100
--- main/goodies/conditional_expression/code/lambda_expression.py
+++ main/goodies/conditional_expression/code/lambda_expression.py
@@ -1,3 +1,6 @@
+""" Conditional expressions are very useful within lambda expressions
+"""
+
 func = lambda x: 'even' if x % 2 == 0 else 'odd'
 
 print( func(2) ) # even
