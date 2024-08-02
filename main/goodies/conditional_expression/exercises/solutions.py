e5aa003081153ba8b9a019d320a30093af20a767
minte9 <catalinprescure@gmail.com>
2024-08-02 15:44:50 +0300

    python-pages update

--- main/goodies/conditional_expression/exercises/solutions.py
+++ main/goodies/conditional_expression/exercises/solutions.py
@@ -9,3 +9,16 @@ print(b) # 100
 a = -100
 b = 100 if a >=0 else -1
 print(b) # -1
+
+
+""" 2. Define the function to get the factorial of a number.   
+       Use conditional expression. 
+"""
+
+def factorial(n):
+    return 1 if n==0 else factorial(n-1) * n
+
+print("Factorial(0): ", factorial(0))  # 1
+print("Factorial(1): ", factorial(1))  # 1
+print("Factorial(2): ", factorial(2))  # 2
+print("Factorial(2): ", factorial(3))  # 6
