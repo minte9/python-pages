5ce3121db486034b4cc1b3d6345a52efd0143d7f
minte9 <66180214+minte9@users.noreply.github.com>
2024-08-08 15:36:24 +0300

    Update transactions_report.py

--- main/packages/itertools/transactions_report.py
+++ main/packages/itertools/transactions_report.py
@@ -1,7 +1,9 @@
 """ Transactions report
     Generate a simple daily report of total credits and debits.
-    
+
 Itemgetter creates a callable (function) that retrive a specific item.
+    https://docs.python.org/3/library/operator.html
+
 You need to assign the list of a generator to a new variable.
 Once you convert group (generator) to a list and iterate over it, you cannot iterate over it again.
 """
