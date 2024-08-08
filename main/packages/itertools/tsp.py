e14474344f3df11025498a91e0360a0b9e35f78e
minte9 <66180214+minte9@users.noreply.github.com>
2024-08-08 09:40:19 +0300

    Update tsp.py

--- main/packages/itertools/tsp.py
+++ main/packages/itertools/tsp.py
@@ -1,7 +1,7 @@
 """ Traveling Salesman Problem (TSP)
-Find the shortest route that visits a list of cities.
-We can generate all posible routes and find the shortest that visits 
-a list of cities and return to the original city.
+    Find the shortest route that visits a list of cities.
+    We can generate all posible routes and find the shortest that visits 
+    a list of cities and return to the original city.
 """
 
 from itertools import permutations
@@ -51,3 +51,46 @@ for route in routes:
         shortest_route = route
 
 print(f"The shortest route is {shortest_route} with a distance of {min_distance}")
+
+"""
+    ic| distances: {('A', 'B'): 10,
+                    ('A', 'C'): 15,
+                    ('A', 'D'): 20,
+                    ('B', 'A'): 10,
+                    ('B', 'C'): 35,
+                    ('B', 'D'): 25,
+                    ('C', 'A'): 15,
+                    ('C', 'B'): 35,
+                    ('C', 'D'): 30,
+                    ('D', 'A'): 20,
+                    ('D', 'B'): 25,
+                    ('D', 'C'): 30}
+
+    ic| routes: <itertools.permutations object at 0x0000023204ED4040>
+    ic| route: ('A', 'B', 'C', 'D'), curr_distance: 95
+    ic| route: ('A', 'B', 'D', 'C'), curr_distance: 80
+    ic| route: ('A', 'C', 'B', 'D'), curr_distance: 95
+    ic| route: ('A', 'C', 'D', 'B'), curr_distance: 80
+    ic| route: ('A', 'D', 'B', 'C'), curr_distance: 95
+    ic| route: ('A', 'D', 'C', 'B'), curr_distance: 95
+    ic| route: ('B', 'A', 'C', 'D'), curr_distance: 80
+    ic| route: ('B', 'A', 'D', 'C'), curr_distance: 95
+    ic| route: ('B', 'C', 'A', 'D'), curr_distance: 95
+    ic| route: ('B', 'C', 'D', 'A'), curr_distance: 95
+    ic| route: ('B', 'D', 'A', 'C'), curr_distance: 95
+    ic| route: ('B', 'D', 'C', 'A'), curr_distance: 80
+    ic| route: ('C', 'A', 'B', 'D'), curr_distance: 80
+    ic| route: ('C', 'A', 'D', 'B'), curr_distance: 95
+    ic| route: ('C', 'B', 'A', 'D'), curr_distance: 95
+    ic| route: ('C', 'B', 'D', 'A'), curr_distance: 95
+    ic| route: ('C', 'D', 'A', 'B'), curr_distance: 95
+    ic| route: ('C', 'D', 'B', 'A'), curr_distance: 80
+    ic| route: ('D', 'A', 'B', 'C'), curr_distance: 95
+    ic| route: ('D', 'A', 'C', 'B'), curr_distance: 95
+    ic| route: ('D', 'B', 'A', 'C'), curr_distance: 80
+    ic| route: ('D', 'B', 'C', 'A'), curr_distance: 95
+    ic| route: ('D', 'C', 'A', 'B'), curr_distance: 80
+    ic| route: ('D', 'C', 'B', 'A'), curr_distance: 95
+
+    The shortest route is ('A', 'B', 'D', 'C') with a distance of 80
+"""
