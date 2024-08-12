50071b33a2b9fb59c53790620f3479a640ccb2a6
minte9 <catalinprescure@gmail.com>
2024-08-12 13:38:39 +0300

    python-pages update

---
+++ main/basics/hello_world/code/operators.py
@@ -0,0 +1,10 @@
+""" Operators
+The + operator concatenates two strings
+The * operator performs repetitions on strings
+"""
+
+a = 'Hello'
+b = 'World'
+
+print(a + ' ' + b) # Hello World
+print(a*3) # HelloHelloHello
