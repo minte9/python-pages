727466351941366e3e1dce032efe130c101d7206
minte9 <catalinprescure@gmail.com>
2024-08-12 14:30:20 +0300

    python-pages update

---
+++ main/basics/hello_world/exercises/readme.md
@@ -0,0 +1,5 @@
+### Exercises / Hello World
+
+1. Create a variable a and give it a value.
+2. What happens when you type a statement?
+3. Give an example of using the repetion operator
---
+++ main/basics/hello_world/exercises/solutions.py
@@ -0,0 +1,19 @@
+""" 1. Create a variable a and give it a value.
+"""
+
+a = 10
+
+""" 2. What happens when you type a statement?
+"""
+
+print(a) # 10
+    # The python interpreter execute the statement
+
+
+""" 3. Give an example of using the repetion operator
+"""
+
+a = "Hello"
+b = a * 3
+print(b)
+    # HelloHelloHello
