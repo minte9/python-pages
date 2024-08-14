ff3077f84746d386c32328dd637450bcd6f6e08d
minte9 <catalinprescure@gmail.com>
2024-08-14 15:39:18 +0300

    python-pages update

--- main/strings/slice/imutable.py
+++
@@ -1,6 +0,0 @@
-# You can't change a string ...
-# it is imutable
-
-str = "Hello World"
-str[0] = "J"
-    # TypeError: 'str' object does not support item assignment
--- main/strings/slice/slice.py
+++
@@ -1,7 +0,0 @@
-# Slice a segment from a string
-
-str = "banana"
-
-print(str[0:2]) # ba
-print(str[:2]) # ba
-print(str[2:]) # nana
