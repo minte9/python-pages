e0b240ab81ebe3a5fd0d65d4bb9da235e1150596
minte9 <catalinprescure@gmail.com>
2024-08-14 15:46:21 +0300

    python-pages update

--- main/basics/hello_world/code/hello_world.py
+++ main/basics/hello_world/code/hello_world.py
@@ -1,5 +1,15 @@
-""" python hello.py
+""" An assignment creates a variable and gives it value
+    python hello_world.py
 """
 
-print('Hello World') 
+message = 'Hello World'
+print(message) 
     # Hello World
+
+n = 17
+n = n + 24
+print(n) 
+
+# You can assign multiple variables in one line
+a, b, c = [1, 2, 3]
+print(a, b, c)
