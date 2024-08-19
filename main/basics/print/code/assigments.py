d05d2bf85909d20b0ee8aa76ef33b9e21f59a7e9
minte9 <catalinprescure@gmail.com>
2024-08-19 17:27:01 +0300

    python-pages update

--- main/basics/print/code/assigments.py
+++ main/basics/print/code/assigments.py
@@ -1,11 +1,12 @@
 """ An assignment creates a variable and gives it value
-    You can assign multiple variables in one line
 """
 
 n = 17
 n = n + 24
 print(n) 
 
+
+# You can assign multiple variables in one line
 a, b, c = 1, 2, 3
 print(a, b, c)
 
