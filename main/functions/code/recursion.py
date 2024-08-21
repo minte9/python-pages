57d8f32e136a18f878571d8eaa53b7f6a8afabf1
minte9 <catalinprescure@gmail.com>
2024-08-21 12:42:55 +0300

    python-pages update

---
+++ main/functions/code/recursion.py
@@ -0,0 +1,43 @@
+""" It is legal for a function to call itself 
+    It is one of the most magical things a program can do
+"""
+
+def countdown(n):
+    if n <= 0:
+        print ("End")
+    else:
+        print(n, end=" ")
+        countdown(n-1)
+
+countdown(3)
+
+"""
+    3 2 1 End
+
+    Execution Flow
+    ---------------------------------------------------
+
+    countdown executions begins with n=3 ...
+    since n is greater than 0 it outputs 3 ..
+    then it calls itself ...
+
+        countdown executions begins with n=2 ...
+        since n is greater than 0 it outputs 2 ..
+        then it calls itself ...
+
+            countdown executions begins with n=1 ...
+            since n is greater than 0 it outputs 1 ..
+            then it calls itself ...
+
+                countdown executions begins with n=0 ...
+                since n is not greater than 0 it outputs Time! ..
+                then returns ...
+
+            the countdown that got n=1 returns
+
+        the countdown that got n=2 returns
+
+    the countdown that got n=3 returns
+
+    After that we are back in the __main__
+"""
