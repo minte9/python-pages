51251f3bfbc789612228e6b926371fe5129186f8
minte9 <catalinprescure@gmail.com>
2024-08-21 13:14:10 +0300

    python-pages update

--- main/functions/recursion/code/execution_flow.py
+++ main/functions/recursion/code/execution_flow.py
@@ -4,28 +4,28 @@
 def countdown(n):
     if n <= 0:
         print ("End")
-    else:
-        print(n, end=" ")
-        countdown(n-1)
+    
+    print(n, end=" ")
+    countdown(n-1)
 
 countdown(3)   # 3 2 1 End
 
 """
     countdown executions begins with n=3 ...
-    since n is greater than 0 it outputs 3 ..
+    since n is greater than 0 it outputs n (3) ..
     then it calls itself ...
 
-        countdown executions begins with n=2 ...
-        since n is greater than 0 it outputs 2 ..
+        begins with n=2 ...
+        since n > 0 it outputs n (2) ..
         then it calls itself ...
 
-            countdown executions begins with n=1 ...
-            since n is greater than 0 it outputs 1 ..
+            begins with n=1 ...
+            since n > 0 it outputs n (1) ..
             then it calls itself ...
 
-                countdown executions begins with n=0 ...
-                since n is not greater than 0 it outputs Time! ..
-                then returns ...
+                begins with n=0 ...
+                since n <= 0 it outputs End ..
+                then returns None
 
             the countdown that got n=1 returns
 
