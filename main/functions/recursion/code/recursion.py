1ea1a08340e98cc4ab4072fd083f18bd9d698996
minte9 <catalinprescure@gmail.com>
2024-08-21 13:09:58 +0300

    python-pages update

--- main/functions/recursion/code/recursion.py
+++ main/functions/recursion/code/recursion.py
@@ -13,31 +13,4 @@ countdown(3)
 
 """
     3 2 1 End
-
-    Execution Flow
-    ---------------------------------------------------
-
-    countdown executions begins with n=3 ...
-    since n is greater than 0 it outputs 3 ..
-    then it calls itself ...
-
-        countdown executions begins with n=2 ...
-        since n is greater than 0 it outputs 2 ..
-        then it calls itself ...
-
-            countdown executions begins with n=1 ...
-            since n is greater than 0 it outputs 1 ..
-            then it calls itself ...
-
-                countdown executions begins with n=0 ...
-                since n is not greater than 0 it outputs Time! ..
-                then returns ...
-
-            the countdown that got n=1 returns
-
-        the countdown that got n=2 returns
-
-    the countdown that got n=3 returns
-
-    After that we are back in the __main__
 """
