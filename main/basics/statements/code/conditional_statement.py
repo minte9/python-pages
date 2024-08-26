113a04cfcfc5706281bf6e816ff63499edb709a4
minte9 <catalinprescure@gmail.com>
2024-08-26 14:20:41 +0300

    python-pages update

--- main/basics/statements/code/conditional_statement.py
+++ main/basics/statements/code/conditional_statement.py
@@ -2,11 +2,23 @@
     We can check condtions and change the behavior
 """
 
-N = 20
+n = 20
 
-if N % 10 == 0:
-    print("N is divisible by 10")
-elif N % 10 > 0:
-    print("N is not divisible by 10") 
+if n % 10 == 0:
+    print("n is divisible by 10")
+elif n % 10 > 0:
+    print("n is not divisible by 10") 
 else:
     pass
+
+
+# Conditional expression (more concisely)
+a = 0
+msg = "positive" if a >= 0 else "negative"
+print("a is", msg)
+
+
+# Conditional expressions are very useful within lambda expressions
+func = lambda x: 'even' if x % 2 == 0 else 'odd'
+print(func(2))
+print(func(3))
