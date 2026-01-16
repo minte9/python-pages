3aa83f21769cbeded370426543134cb55ab48e91
minte9 <catalinprescure@gmail.com>
2026-01-16 22:28:41 +0200

    python-pages update

--- main/goodies/list_comprehension/code/list_comprehension.py
+++ main/goodies/list_comprehension/code/list_comprehension.py
@@ -1,21 +1,40 @@
-""" We can write for loop more concisely using List Comprehension
+""" 
+    List comprehension 
 """
+A = [i for i in range(3)]
 
-# Input string
-S = "abcd"
+# Equivalent loop
+E = []
+for i in range(3):
+    E.append(i)
 
-# Loop (easier to debug)
-R = []
-for s in S:
-    if s in ['a', 'b', 'c']:
-        R.append(s.capitalize())
-print(R)
+print(A)  # [0, 1, 2]
+print(E)  # [0, 1, 2]
 
-# List comprehension (concise)
-R = [s.capitalize() for s in S if s in ['a', 'b', 'c']]
-print(R)
+
+""" 
+    Nested list comprehesion 
+"""
+M = [[i for i in range(3)] for j in range(3)]
+
+# Equivalent loop
+L = []
+for j in range(3):
+    L.append([i for i in range(3)])
+
+print(M)  # [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+print(L)  # [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+
 
 """
-    ['A', 'B', 'C']
-    ['A', 'B', 'C']
+    Permutations
 """
+perms = [
+    [i, j, k]
+    for i in range(3)
+    for j in range(3)
+    for k in range(3)
+    if i != j and j != k and k != i
+]
+
+print(perms)  # [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]]
